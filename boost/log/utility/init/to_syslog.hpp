70f01b046e84bcd4c20e7a982b7ec61ba26e36da
Andreas Baumann <abaumann@yahoo.com>
2010-03-28 15:50:59 +0200

    merged local and windows variant of init_log_to_syslog

--- boost/log/utility/init/to_syslog.hpp
+++ boost/log/utility/init/to_syslog.hpp
@@ -44,8 +44,6 @@
 
 #include <boost/log/keywords/custom_severity_mapping.hpp>
 
-#include <boost/log/unused.hpp>
-
 //! \cond
 #ifndef BOOST_LOG_NO_THREADS
 #define BOOST_LOG_SYSLOG_SINK_FRONTEND_INTERNAL sinks::synchronous_sink
@@ -64,7 +62,7 @@ namespace aux {
 
 template< typename CustomEnumT >
 inline typename sinks::syslog::custom_severity_mapping< CustomEnumT > acquire_custom_severity_mapping( sinks::syslog::custom_severity_mapping< CustomEnumT > const& mapping )
-{
+ {
     return mapping;
 }
 
@@ -75,7 +73,7 @@ inline void setup_custom_severity_mapping(BackendT&, ArgsT const&, mpl::true_)
 }
 
 template< typename BackendT, typename ArgsT >
-inline void setup_custom_severity_mapping(BackendT& b UNUSED, ArgsT const& args UNUSED, mpl::false_)
+inline void setup_custom_severity_mapping(BackendT& b, ArgsT const& args, mpl::false_)
 {
     b.set_severity_mapper(aux::acquire_custom_severity_mapping(args[keywords::custom_severity_mapping]));
 }
@@ -176,6 +174,7 @@ BOOST_PP_REPEAT_FROM_TO(1, BOOST_LOG_MAX_PARAMETER_ARGS, BOOST_LOG_INIT_LOG_TO_S
  * \param args A number of named arguments. The following parameters are supported:
  *             \li \c use_impl Specify native for openlog/syslog API calls or udp_socket_based for the internal version.
  *             \li \c facility The syslog facility to log to
+ *             \li \c custom mapping of an enum used for severity to the syslog severities (which are fix)
  *             \li \c filter Specifies a filter to install into the sink. May be a string that represents a filter,
  *                           or a filter lambda expression.
  *             \li \c format Specifies a formatter to install into the sink. May be a string that represents a formatter,
