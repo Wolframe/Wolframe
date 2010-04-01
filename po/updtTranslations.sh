80b80f1d2c8e3a13863f8efda49ab400aa2c56f9
Mihai Barbos <mihai.barbos@gmail.com>
2010-04-01 15:02:58 +0200

    The message extraction script should now do a better job

--- po/updtTranslations.sh
+++ po/updtTranslations.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+# ${DOMAIN}.pot is used as the extracted message file
 DOMAIN="SMERP"
 LOCALES="ro de"
 EXTENSIONS="hpp cpp"
 
 for ext in ${EXTENSIONS}
 do
-    ls ../src/ | grep "\.${ext}$" |
-    ( while read file
+	ls ../src/ | grep "\.${ext}$" |
+	( while read file
 	do
-    	    xgettext -d ${DOMAIN} -s -D .. -o ${file}.pot src/${file}
-            echo "Processing: ${file}"
-    	    if [ -f ${file}.pot ]; then
-        	    for loc in ${LOCALES}; do
-        		echo "Merging ${file}.pot to ${loc} message file"
-			msgmerge -s -U ${loc}.po ${file}.pot
-    		    done
-		    rm -v ${file}.pot
-    	    fi
+		echo "Processing: ${file}"
+		if [ -f ${DOMAIN}.pot ]; then
+			xgettext -j -d ${DOMAIN} -F -D .. -o ${DOMAIN}.pot src/${file}
+		else
+			xgettext -d ${DOMAIN} -F -D .. -o ${DOMAIN}.pot src/${file}
+		fi
 	done
-    )
+	)
 done
+
+if [ -f ${DOMAIN}.pot ]; then
+	for loc in ${LOCALES}; do
+		echo "Merging ${DOMAIN}.pot to ${loc} message file"
+		msgmerge -v -F -U ${loc}.po ${DOMAIN}.pot
+	done
+	rm -v ${DOMAIN}.pot
+fi
