9483a0072c5088238b80ecb2ba6023bafe66d721
Mihai Barbos <mihai.barbos@gmail.com>
2010-09-05 17:01:22 +0200

    Added gTest tests for Version class

--- tests/versionTest.cpp
+++ tests/versionTest.cpp
@@ -2,10 +2,9 @@
 // version class tests
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "version class test"
+#define BOOST_TEST_MODULE "version class"
 
 #include <boost/test/unit_test.hpp>
-
 
 #include <cstdlib>
 #include <ctime>
@@ -16,27 +15,57 @@
 
 #define	MAX_STRING	128
 
+struct F {
+	F()	{
+		srand((unsigned)time(0));
+		major = (unsigned short)( rand() % USHRT_MAX );
+		minor = (unsigned short)( rand() % USHRT_MAX );
+		revision = (unsigned short)( rand() % USHRT_MAX );
+		build = (unsigned)( rand() % UINT_MAX );
+		if ( !build )
+			build++;
 
-BOOST_AUTO_TEST_CASE( _version_ )
-{
+		ver = new _SMERP::Version( major, minor, revision, build );
+		ver0 = new _SMERP::Version( major, minor, revision );
+		sprintf( verStr, "%d.%d.%d.%u", major, minor, revision, build );
+
+		BOOST_TEST_MESSAGE( "setup test versions" );
+	}
+
+	~F()	{
+		delete ver0;
+		delete ver;
+		BOOST_TEST_MESSAGE( "teardown fixture" );
+	}
+
 	unsigned short	major, minor, revision;
 	unsigned	build;
+	_SMERP::Version	*ver, *ver0;
 	char		verStr[MAX_STRING];
+};
 
-	srand((unsigned)time(0));
-	major = (unsigned short)( rand() % USHRT_MAX );
-	minor = (unsigned short)( rand() % USHRT_MAX );
-	revision = (unsigned short)( rand() % USHRT_MAX );
-	build = (unsigned)( rand() % UINT_MAX );
 
-	_SMERP::Version ver( major, minor, revision, build );
-	sprintf( verStr, "%d.%d.%d.%u", major, minor, revision, build );
+BOOST_FIXTURE_TEST_SUITE( _version_, F )
 
-	BOOST_CHECK_EQUAL( ver.major(), major );
-	BOOST_CHECK_EQUAL( ver.minor(), minor );
-	BOOST_CHECK_EQUAL( ver.revision(), revision );
-	BOOST_CHECK_EQUAL( ver.build(), build );
-	BOOST_CHECK_EQUAL( ver.toString().c_str(), verStr );
-	std::cout << ver.toString();
+BOOST_AUTO_TEST_CASE( members )
+{
+	BOOST_CHECK_EQUAL( ver->major(), major );
+	BOOST_CHECK_EQUAL( ver->minor(), minor );
+	BOOST_CHECK_EQUAL( ver->revision(), revision );
+	BOOST_CHECK_EQUAL( ver->build(), build );
+	BOOST_CHECK_EQUAL( ver->toString().c_str(), verStr );
 }
 
+BOOST_AUTO_TEST_CASE( operators )
+{
+	BOOST_CHECK( *ver != *ver0 );
+	BOOST_CHECK( *ver > *ver0 );
+	BOOST_CHECK( *ver >= *ver0 );
+	BOOST_CHECK( !( *ver == *ver0 ));
+	BOOST_CHECK( !( *ver < *ver0 ));
+	BOOST_CHECK( !( *ver <= *ver0 ));
+
+	std::cout << ver->toString();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
