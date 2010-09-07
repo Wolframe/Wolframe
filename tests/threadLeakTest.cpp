bc3aab9f69b4284ff900787a073b14b9d8120e2b
Andreas Baumann <abaumann@yahoo.com>
2010-09-07 16:39:43 +0200

    started to add a valgrind suppression file adapted gitignore forgot to add theadLeakTest

---
+++ tests/threadLeakTest.cpp
@@ -0,0 +1,25 @@
+#include <boost/thread.hpp>
+
+void
+someWork ()
+{
+  double a;
+  for (int i = 0; i < 1000; i++)
+    {
+      a += log ((double) i);
+    }
+}
+
+int
+main ()
+{
+  for (int i = 0; i < 10; ++i)
+    {
+      boost::thread_group pool;
+      for (int j = 0; j < 100; ++j)
+	{
+	  pool.create_thread (someWork);
+	}
+      pool.join_all ();
+    }
+}
