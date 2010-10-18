5aae17fb2d121edba90339b3c74f85a57f1a40b6
Andreas Baumann <abaumann@yahoo.com>
2010-10-18 10:22:37 +0200

    fixed building on Windows with no WITH_BOOST_LOG

--- makefiles/nmake/config.mk
+++ makefiles/nmake/config.mk
@@ -14,9 +14,9 @@
 # newer versions of Visual Studio integrate the header files of the SDK
 # some versions of Visual Studio miss the mc.exe binary
 
-#PLATFORM_SDK_DIR = C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
+PLATFORM_SDK_DIR = C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
 #PLATFORM_SDK_DIR = C:\Programme\Microsoft Platform SDK for Windows Server 2003 R2
-PLATFORM_SDK_DIR = D:\Programme\Microsoft Platform SDK
+#PLATFORM_SDK_DIR = D:\Programme\Microsoft Platform SDK
 
 # Boost (http://www.boostpro.com/download)
 ###########################################
@@ -27,14 +27,14 @@ BOOST_VERSION=1_42
 
 # base dir where boost is installed
 
-#BOOST_DIR=C:\Program Files\boost\boost_$(BOOST_VERSION)
+BOOST_DIR=C:\Program Files\boost\boost_$(BOOST_VERSION)
 #BOOST_DIR=C:\Programme\boost\boost_$(BOOST_VERSION)
-BOOST_DIR=D:\Programme\boost\boost_$(BOOST_VERSION)
+#BOOST_DIR=D:\Programme\boost\boost_$(BOOST_VERSION)
 
 # visual studio version used for compiling
 
-#BOOST_VC_VER=vc90
-BOOST_VC_VER=vc80
+BOOST_VC_VER=vc90
+#BOOST_VC_VER=vc80
 
 # TODO: probe those
 BOOST_MT=-mt
@@ -42,9 +42,9 @@ BOOST_MT=-mt
 # OpenSSL (http://www.slproweb.com/products/Win32OpenSSL.html)
 ##############################################################
 
-#OPENSSL_DIR=C:\Program Files\OpenSSL
+OPENSSL_DIR=C:\Program Files\OpenSSL
 #OPENSSL_DIR=C:\Programme\OpenSSL
-OPENSSL_DIR=D:\Programme\OpenSSL
+#OPENSSL_DIR=D:\Programme\OpenSSL
 
 # LUA sources (for LUA handler)
 # build it from sources with:
