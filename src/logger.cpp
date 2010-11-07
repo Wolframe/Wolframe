e65f7ad67500efeb6bee5429caab8f1be0539784
Andreas Baumann <abaumann@yahoo.com>
2010-11-07 15:48:25 +0100

    first moves towards a new logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -2,8 +2,33 @@
 // logger.cpp
 //
 
+#define NO_LOG_MACROS
 #include "logger.hpp"
 
+#ifndef _WIN32
+#include <syslog.h>
+#endif // _WIN32
+
 namespace _SMERP {
+
+#ifndef _WIN32
+	int SyslogBackend::levelToSyslogLevel( const LogLevel::Level level )	{
+		switch( level )	{
+			case _SMERP::LogLevel::LOGLEVEL_DATA:
+			case _SMERP::LogLevel::LOGLEVEL_TRACE:
+			case _SMERP::LogLevel::LOGLEVEL_DEBUG:		return LOG_DEBUG;
+			case _SMERP::LogLevel::LOGLEVEL_INFO:		return LOG_INFO;
+			case _SMERP::LogLevel::LOGLEVEL_NOTICE:		return LOG_NOTICE;
+			case _SMERP::LogLevel::LOGLEVEL_WARNING:	return LOG_WARNING;
+			case _SMERP::LogLevel::LOGLEVEL_ERROR:		return LOG_ERR;
+			case _SMERP::LogLevel::LOGLEVEL_SEVERE:
+			case _SMERP::LogLevel::LOGLEVEL_CRITICAL:	return LOG_CRIT;
+			case _SMERP::LogLevel::LOGLEVEL_ALERT:		return LOG_ALERT;
+			case _SMERP::LogLevel::LOGLEVEL_FATAL:		return LOG_EMERG;
+			case _SMERP::LogLevel::LOGLEVEL_UNDEFINED:	return LOG_ERR;
+		}
+		return LOG_ERR;
+	}
+#endif // _WIN32
 
 } // namespace _SMERP
--- src/logger.hpp
+++ src/logger.hpp
@@ -10,23 +10,77 @@
 
 #include <ostream>
 
+#ifndef _WIN32
+#include <syslog.h>
+#endif // _WIN32
+
 namespace _SMERP {
 
-	class LogBackend
+	class ConsoleLogBackend
 	{
 	public:
-		LogBackend()	{ logLevel_ = _SMERP::LogLevel::LOGLEVEL_NOTICE; }
-		~LogBackend()	{ /* free logger resources here */ };
+		ConsoleLogBackend( ) { logLevel_ = _SMERP::LogLevel::LOGLEVEL_NOTICE; }
+		~ConsoleLogBackend( ) { }
 
-		void setLevel( LogLevel::Level level )	{
+		void setLevel( const LogLevel::Level level )	{
 			logLevel_ = level;
 		}
-		void log( LogLevel::Level level, const std::string& msg )	{
+		
+		inline void log( const LogLevel::Level level, const std::string& msg )	{
 			if ( level >= logLevel_ )
 				std::cerr << level << ": " << msg << std::endl;
 		}
 	private:
-		LogLevel::Level	logLevel_;
+		LogLevel::Level	logLevel_;		
+	};
+
+#ifndef _WIN32
+	class SyslogBackend
+	{
+	public:
+		SyslogBackend( ) { logLevel_ = _SMERP::LogLevel::LOGLEVEL_ERROR; }
+		~SyslogBackend( ) { }
+		
+		void setLevel( const LogLevel::Level level )	{
+			logLevel_ = level;
+		}
+		
+		inline void log( const LogLevel::Level level, const std::string& msg )	{
+			if ( level >= logLevel_ )
+				syslog( levelToSyslogLevel( level ), "%s", msg.c_str( ) );
+		}
+
+	private:
+		LogLevel::Level logLevel_;		
+
+		int levelToSyslogLevel( const LogLevel::Level level );
+	};
+#endif // _WIN32	
+
+	class LogBackend
+	{
+	public:
+		LogBackend()	{  }
+		~LogBackend()	{ /* free logger resources here */ }
+
+		void setLevel( const LogLevel::Level level )	{
+			consoleLogger_.setLevel( level );
+#ifndef _WIN32
+			syslogLogger_.setLevel( level );
+#endif // _WIN32
+		}
+		
+		inline void log( const LogLevel::Level level, const std::string& msg )	{
+			consoleLogger_.log( level, msg );
+#ifndef _WIN32
+			syslogLogger_.log( level, msg );
+#endif // _WIN32
+		}
+	private:
+		ConsoleLogBackend consoleLogger_;
+#ifndef _WIN32
+		SyslogBackend syslogLogger_;
+#endif // _WIN32	
 	};
 
 
@@ -54,6 +108,16 @@ namespace _SMERP {
 
 extern _SMERP::LogBackend	logBack;
 
+#ifndef NO_LOG_MACROS
+
+#ifndef _WIN32
+#undef LOG_DEBUG
+#undef LOG_INFO
+#undef LOG_NOTICE
+#undef LOG_WARNING
+#undef LOG_ALERT
+#endif // _WIN32
+
 // shortcut macros
 #define LOG_DATA	_SMERP::Logger( logBack ).Get( _SMERP::LogLevel::LOGLEVEL_DATA )
 #define LOG_TRACE	_SMERP::Logger( logBack ).Get( _SMERP::LogLevel::LOGLEVEL_TRACE )
@@ -66,5 +130,7 @@ extern _SMERP::LogBackend	logBack;
 #define LOG_CRITICAL	_SMERP::Logger( logBack ).Get( _SMERP::LogLevel::LOGLEVEL_CRITICAL )
 #define LOG_ALERT	_SMERP::Logger( logBack ).Get( _SMERP::LogLevel::LOGLEVEL_ALERT )
 #define LOG_FATAL	_SMERP::Logger( logBack ).Get( _SMERP::LogLevel::LOGLEVEL_FATAL )
+
+#endif // NO_LOG_MACROS
 
 #endif // _LOGGER_HPP_INCLUDED
