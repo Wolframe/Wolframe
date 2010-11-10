7f584f25dc0112d212401542f469f8787f328884
Andreas Baumann <abaumann@yahoo.com>
2010-11-10 08:29:57 +0100

    -

--- makefiles/nmake/config.mk
+++ makefiles/nmake/config.mk
@@ -15,8 +15,8 @@
 # some versions of Visual Studio miss the mc.exe binary
 
 #PLATFORM_SDK_DIR = C:\Programme\Microsoft Platform SDK
-#PLATFORM_SDK_DIR = C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
-PLATFORM_SDK_DIR = D:\Programme\Microsoft Platform SDK
+PLATFORM_SDK_DIR = C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
+#PLATFORM_SDK_DIR = D:\Programme\Microsoft Platform SDK
 
 # Boost (http://www.boostpro.com/download)
 ###########################################
@@ -28,13 +28,13 @@ BOOST_VERSION=1_44
 # base dir where boost is installed
 
 #BOOST_DIR=C:\Programme\boost\boost_$(BOOST_VERSION)
-#BOOST_DIR=C:\Program Files\boost\boost_$(BOOST_VERSION)
-BOOST_DIR=D:\Programme\boost\boost_$(BOOST_VERSION)
+BOOST_DIR=C:\Program Files\boost\boost_$(BOOST_VERSION)
+#BOOST_DIR=D:\Programme\boost\boost_$(BOOST_VERSION)
 
 # visual studio version used for compiling
 
-BOOST_VC_VER=vc80
-#BOOST_VC_VER=vc90
+#BOOST_VC_VER=vc80
+BOOST_VC_VER=vc90
 
 # TODO: probe those
 BOOST_MT=-mt
@@ -43,5 +43,5 @@ BOOST_MT=-mt
 ##############################################################
 
 #OPENSSL_DIR=C:\Programme\OpenSSL-1.0.0a
-#OPENSSL_DIR=C:\Program Files\OpenSSL
-OPENSSL_DIR=D:\Programme\OpenSSL
+OPENSSL_DIR=C:\Program Files\OpenSSL
+#OPENSSL_DIR=D:\Programme\OpenSSL
