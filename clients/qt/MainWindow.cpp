ceeb3f7e1da8308a67aab1fdbdb6023f5a6923d7
Andreas Baumann <abaumann@yahoo.com>
2010-11-15 21:25:43 +0100

    out of battery check-in

--- clients/qt/MainWindow.cpp
+++ clients/qt/MainWindow.cpp
@@ -45,6 +45,54 @@ void MainWindow::populateThemesMenu( )
 
 void MainWindow::loadTheme( QString theme )
 {
+#if 0
+	current_theme = theme;
+
+	QWidget *old_ui = ui;
+	QUiLoader loader;
+
+	// go to theme directory
+	QString theme_folder( QLatin1String( "themes/" ) + theme + QLatin1Char( '/' ) );
+	loader.setWorkingDirectory( theme_folder );
+
+	// load the main window
+	QFile file( theme_folder + QLatin1String( "Test1.ui" ) );
+	file.open( QFile::ReadOnly );
+	ui = loader.load( &file, this );
+	file.close( );
+
+	// set stylesheet of the application (has impact on the whole application)
+	QFile qss( theme_folder + QLatin1String( "Test1.qss" ) );
+	qApp->setStyleSheet( QLatin1String( "file:///" ) + QFileInfo( qss ).absoluteFilePath( ) );
+
+	// initialize menu actions and connects signals to slots
+	initialize( );
+
+	// copy over the location of the old window to the new one
+	// also copy over the current form, don't destroy the old ui,
+	// events could be outstanding (deleteLater marks the widget
+	// for deletion, will be deleted when returning into the event
+	// loop)
+	if( old_ui ) {
+		ui->move( old_ui->pos( ) );
+		old_ui->hide( );
+		old_ui->deleteLater( );
+	}
+
+	// set preferred base style
+	QStyle *base_style = QStyleFactory::create( ui->property( "themeBaseStyle" ).toString( ) );
+	if( base_style ) {
+		base_style->setParent( ui );
+		ui->setStyle( base_style );
+	}
+
+	// show new UI
+	ui->show( );
+
+#endif
+
+	qApp->setOverrideCursor( Qt::BusyCursor );
+
 // set working directory to theme
 	QString themesFolder( QLatin1String( "themes/" ) + theme + QLatin1Char( '/' ) );
 
@@ -59,11 +107,26 @@ void MainWindow::loadTheme( QString theme )
 	m_ui = loader.load( &file, this );
 	file.close( );
 
-// wire standard actions by name (on_<object>_<signal>)
-	QMetaObject::connectSlotsByName( this );
+// set stylesheet of the application (has impact on the whole application)
+	QFile qss( themesFolder + QLatin1String( "MainWindow.qss" ) );
+	qApp->setStyleSheet( QLatin1String( "file:///" ) + QFileInfo( qss ).absoluteFilePath( ) );
+
+// wire standard actions in the menu by name (on_<object>_<signal>)
+// (true autowiring doesn't work: from now signals to ghost signals everything
+// happens)
+	QAction *actionExit = qFindChild<QAction *>( m_ui, "actionExit" );
+	QObject::connect( actionExit, SIGNAL( triggered( ) ), this, SLOT( on_actionExit_triggered( ) ) );
+
+	QAction *actionAbout = qFindChild<QAction *>( m_ui, "actionAbout" );
+	QObject::connect( actionAbout, SIGNAL( triggered( ) ), this, SLOT( on_actionAbout_triggered( ) ) );
+
+	QAction *actionAboutQt = qFindChild<QAction *>( m_ui, "actionAboutQt" );
+	QObject::connect( actionAboutQt, SIGNAL( triggered( ) ), this, SLOT( on_actionAboutQt_triggered( ) ) );
 
 // remember current theme
 	m_currentTheme = theme;
+
+	qApp->restoreOverrideCursor( );
 }
 
 void MainWindow::themeSelected( QAction *action )
