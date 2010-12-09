b1a3fc2f445e0ff573ce7e6048f96acb2ff273e7
U-Andreas-PC\Andreas <abaumann@yahoo.com>
2010-12-09 11:40:05 +0100

    -

--- makefiles/nmake/config.mk
+++ makefiles/nmake/config.mk
@@ -17,8 +17,8 @@
 #PLATFORM_SDK_DIR = C:\Programme\Microsoft Platform SDK
 #PLATFORM_SDK_DIR = C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
 #PLATFORM_SDK_DIR = D:\Programme\Microsoft Platform SDK
-#PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v7.0
-PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v7.1
+PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v7.0
+#PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v7.1
 
 # Boost (http://www.boostpro.com/download)
 ###########################################
@@ -32,14 +32,14 @@ BOOST_VERSION=1_45
 #BOOST_DIR=C:\Programme\boost\boost_$(BOOST_VERSION)
 #BOOST_DIR=C:\Program Files\boost\boost_$(BOOST_VERSION)
 #BOOST_DIR=D:\Programme\boost\boost_$(BOOST_VERSION)
-#BOOST_DIR=E:\boost_1_45_0
-BOOST_DIR=C:\Cygwin\home\Administrator\boost_1_45_0
+BOOST_DIR=C:\Cygwin\home\Andreas\boost_1_45_0
+#BOOST_DIR=C:\Cygwin\home\Administrator\boost_1_45_0
 
 # visual studio version used for compiling
 
 #BOOST_VC_VER=vc80
-#BOOST_VC_VER=vc90
-BOOST_VC_VER=vc100
+BOOST_VC_VER=vc90
+#BOOST_VC_VER=vc100
 
 # TODO: probe those
 BOOST_MT=-mt
@@ -53,8 +53,8 @@ BOOST_LDFLAGS = /LIBPATH:"$(BOOST_DIR)\stage\lib"
 # OpenSSL (http://www.slproweb.com/products/Win32OpenSSL.html)
 ##############################################################
 
-OPENSSL_DIR=C:\Programme\OpenSSL-1.0.0a
-#OPENSSL_DIR=C:\Program Files\OpenSSL
+#OPENSSL_DIR=C:\Programme\OpenSSL-1.0.0a
+OPENSSL_DIR=C:\Program Files\OpenSSL
 #OPENSSL_DIR=D:\Programme\OpenSSL
 
 # Qt 4 (http://qt.nokia.com/products/)
