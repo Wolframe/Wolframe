08cd341cc8ce9d03ac280cbb1c58143b0d16c07a
Andreas Baumann <abaumann@yahoo.com>
2010-12-29 22:31:21 +0100

    some fixes on Windows, things compile but there are still plently of things disabled or broken

--- include/protocol/buffers.hpp
+++ include/protocol/buffers.hpp
@@ -32,7 +32,7 @@ public:
 //buffer for the currently parsed command. 
 struct CmdBuffer
 {
-   typedef int_least64_t ValueType;    //< stores the command name with a maximum of 10 characters (6 bit per character = case insensitive alpha or digit)
+   typedef boost::int_least64_t ValueType;    //< stores the command name with a maximum of 10 characters (6 bit per character = case insensitive alpha or digit)
    unsigned int pos;               //< current position
    ValueType value;
    enum 
