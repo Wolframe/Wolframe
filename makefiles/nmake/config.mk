c5214d411f650e041f72e37e4ee088a5262d9c23
Andreas Baumann <abaumann@yahoo.com>
2011-01-16 15:36:58 +0100

    small fixes on Windows

--- makefiles/nmake/config.mk
+++ makefiles/nmake/config.mk
@@ -14,10 +14,10 @@
 # newer versions of Visual Studio integrate the header files of the SDK
 # some versions of Visual Studio miss the mc.exe binary
 
-#PLATFORM_SDK_DIR = C:\Programme\Microsoft Platform SDK
+PLATFORM_SDK_DIR = C:\Programme\Microsoft Platform SDK
 #PLATFORM_SDK_DIR = C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
 #PLATFORM_SDK_DIR = D:\Programme\Microsoft Platform SDK
-PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v6.0A
+#PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v6.0A
 #PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v7.0
 #PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v7.1
 
@@ -26,44 +26,44 @@ PLATFORM_SDK_DIR = C:\Program Files\Microsoft SDKs\Windows\v6.0A
 
 # version of the boost library
 
-BOOST_VERSION=1_45
+BOOST_VERSION=1_44
 
 # base dir where boost is installed
 
-#BOOST_DIR=C:\Programme\boost\boost_$(BOOST_VERSION)
+BOOST_DIR=C:\Programme\boost\boost_$(BOOST_VERSION)
 #BOOST_DIR=C:\Program Files\boost\boost_$(BOOST_VERSION)
 #BOOST_DIR=D:\Programme\boost\boost_$(BOOST_VERSION)
-BOOST_DIR=C:\Cygwin\home\Andreas\boost_1_45_0
+#BOOST_DIR=C:\Cygwin\home\Andreas\boost_1_45_0
 #BOOST_DIR=C:\Cygwin\home\Administrator\boost_1_45_0
 
 # visual studio version used for compiling
 
-#BOOST_VC_VER=vc80
-BOOST_VC_VER=vc90
+BOOST_VC_VER=vc80
+#BOOST_VC_VER=vc90
 #BOOST_VC_VER=vc100
 
 # TODO: probe those
 BOOST_MT=-mt
 
 # preinstalled binaries
-#BOOST_LDFLAGS = /LIBPATH:"$(BOOST_DIR)\lib"
+BOOST_LDFLAGS = /LIBPATH:"$(BOOST_DIR)\lib"
 
 # self-compiled
-BOOST_LDFLAGS = /LIBPATH:"$(BOOST_DIR)\stage\lib"
+#BOOST_LDFLAGS = /LIBPATH:"$(BOOST_DIR)\stage\lib"
 
 # OpenSSL (http://www.slproweb.com/products/Win32OpenSSL.html)
 ##############################################################
 
-#OPENSSL_DIR=C:\Programme\OpenSSL-1.0.0a
-OPENSSL_DIR=C:\Programme\OpenSSL
+OPENSSL_DIR=C:\Programme\OpenSSL-1.0.0a
+#OPENSSL_DIR=C:\Programme\OpenSSL
 #OPENSSL_DIR=C:\Program Files\OpenSSL
 #OPENSSL_DIR=D:\Programme\OpenSSL
 
 # gtest (http://code.google.com/p/googletest)
 #############################################
 
-#GTEST_DIR=C:\Cygwin\home\Administrator\gtest-1.5.0
-GTEST_DIR=C:\Cygwin\home\Andreas\gtest-1.5.0
+GTEST_DIR=C:\Cygwin\home\Administrator\gtest-1.5.0
+#GTEST_DIR=C:\Cygwin\home\Andreas\gtest-1.5.0
 
 GTEST_INCLUDE_DIRS=$(GTEST_DIR)\include
 GTEST_LIB_DIR=$(GTEST_DIR)\src
