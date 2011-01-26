4dd09c44615a83bfd47d232543c93668f91b408a
Andreas Baumann <abaumann@yahoo.com>
2011-01-26 18:00:21 +0100

    started to clean up makefile and config target

--- makefiles/gmake/config.mk
+++ makefiles/gmake/config.mk
@@ -26,14 +26,16 @@
 # base dir where boost is installed
 
 # system version
-BOOST_DIR=/usr
-BOOST_LIB_DIRS=$(BOOST_DIR)/lib
-BOOST_INCLUDE_DIRS=$(BOOST_DIR)/include
+#BOOST_DIR=/usr
+#BOOST_LIB_DIRS=$(BOOST_DIR)/lib
+#BOOST_INCLUDE_DIRS=$(BOOST_DIR)/include
+#BOOST_LIBRARY_TAG=-mt
 
 # self-compiled
 #BOOST_DIR=/home/abaumann/boost_1_45_0
 #BOOST_INCLUDE_DIRS=$(BOOST_DIR)
 #BOOST_LIB_DIR=$(BOOST_DIR)/stage/lib
+#BOOST_LIBRARY_TAG=-mt
 
 # OpenSSL (http://www.slproweb.com/products/Win32OpenSSL.html)
 ##############################################################
