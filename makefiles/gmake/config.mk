4965a3feb61743607de43c4d8a9a052effc82513
Andreas Baumann <abaumann@yahoo.com>
2011-01-31 20:13:59 +0100

    -

--- makefiles/gmake/config.mk
+++ makefiles/gmake/config.mk
@@ -23,67 +23,65 @@
 # Boost (http://www.boostpro.com/download)
 ###########################################
 
-# base dir where boost is installed
-
 # system version
-#BOOST_DIR ?= /usr/local
-#BOOST_LIB_DIR ?= $(BOOST_DIR)/lib2
-#BOOST_INCLUDE_DIR ?= $(BOOST_DIR)/include2
-#BOOST_LIBRARY_TAG ?=
+#BOOST_DIR = /usr/local
+#BOOST_LIB_DIR = $(BOOST_DIR)/lib2
+#BOOST_INCLUDE_DIR = $(BOOST_DIR)/include2
+#BOOST_LIBRARY_TAG =
 
 # self-compiled
-#BOOST_DIR=/home/abaumann/boost_1_45_0
-#BOOST_INCLUDE_DIR=$(BOOST_DIR)
-#BOOST_LIB_DIR=$(BOOST_DIR)/stage/lib
-#BOOST_LIBRARY_TAG=-mt
+#BOOST_DIR = /home/abaumann/boost_1_45_0
+#BOOST_INCLUDE_DIR = $(BOOST_DIR)
+#BOOST_LIB_DIR = $(BOOST_DIR)/stage/lib
+#BOOST_LIBRARY_TAG = -mt
 
 # OpenSSL (http://www.slproweb.com/products/Win32OpenSSL.html)
 ##############################################################
 
 # system version
-OPENSSL_DIR ?= /usr
+#OPENSSL_DIR = /usr
 
 # self-compiled
-#OPENSSL_DIR=/home/user/openssl
+#OPENSSL_DIR = /home/user/openssl
 
 # gtest (http://code.google.com/p/googletest)
 #############################################
 
 # system version
-#GTEST_DIR=/usr
-#GTEST_LIB_DIR=$(GTEST_DIR)/lib
-#GTEST_INCLUDE_DIRS=$(GTEST_DIR)/include
+#GTEST_DIR = /usr
+#GTEST_LIB_DIR = $(GTEST_DIR)/lib
+#GTEST_INCLUDE_DIRS = $(GTEST_DIR)/include
 #GTEST_LIBS = -lgtest
 
 # self-compiled
-#GTEST_DIR=/home/abaumann/gtest-1.5.0
-#GTEST_INCLUDE_DIRS=$(GTEST_DIR)/include
-#GTEST_LIB_DIR=$(GTEST_DIR)/lib/.libs
+#GTEST_DIR = /home/abaumann/gtest-1.5.0
+#GTEST_INCLUDE_DIRS = $(GTEST_DIR)/include
+#GTEST_LIB_DIR = $(GTEST_DIR)/lib/.libs
 #GTEST_LIBS = -lgtest
 
 # Qt 4 (http://qt.nokia.com/products/)
 ######################################
 
 # self-compiled
-#QT_DIR=/home/user/qt
+#QT_DIR = /home/user/qt
 
 # system-wide
-#QT_DIR=/usr
+#QT_DIR = /usr
 
 # usual layout, may differ
-#QT_INCLUDE_DIRS=$(QT_DIR)/include
-#QT_LIB_DIRS=$(QT_DIR)/lib
+#QT_INCLUDE_DIRS = $(QT_DIR)/include
+#QT_LIB_DIRS = $(QT_DIR)/lib
 
 # Lua 5.1
 #########
 
 # self-compiled
-#LUA_DIR=/home/user/lua-5.1.4
+#LUA_DIR = /home/user/lua-5.1.4
 
 # system-wide
-#LUA_DIR=/usr
+#LUA_DIR = /usr
 
 # usual layout
-#LUA_INCLUDE_DIRS=$(LUA_DIR)/include
-#LUA_LIB_DIRS=$(LUA_DIR)/lib
-#LUA_LIBS=-llua
+#LUA_INCLUDE_DIRS = $(LUA_DIR)/include
+#LUA_LIB_DIRS = $(LUA_DIR)/lib
+#LUA_LIBS = -llua
