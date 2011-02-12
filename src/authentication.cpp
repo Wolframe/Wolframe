5474f2d66cc98faea7f712b77758dae50da93da5
Andreas Baumann <abaumann@yahoo.com>
2011-02-12 19:17:18 +0100

    added basis mech enum for authentication

--- src/authentication.cpp
+++ src/authentication.cpp
@@ -4,3 +4,30 @@
 
 #include "authentication.hpp"
 
+namespace _SMERP {
+	namespace Authentication {
+
+Mech::AuthMech str2AuthMech( const std::string s )
+{
+	if( s == "TEXT_FILE" )		return Mech::_SMERP_AUTH_MECH_TEXT_FILE;
+#ifdef _WIN32
+	else if( s == "WINAD" )		return Mech::_SMERP_AUTH_MECH_WINAD;
+	else if( s == "WINKERB" )	return Mech::_SMERP_AUTH_MECH_WINKERB;
+#endif
+#ifdef WITH_OPENLDAP
+	else if( s == "LDAP" )		return Mech::_SMERP_AUTH_MECH_LDAP;
+#endif
+#ifdef WITH_PAM
+	else if( s == "PAM" )		return Mech::_SMERP_AUTH_MECH_PAM;
+#endif
+#ifdef WITH_SASL
+	else if( s == "SASL" )		return Mech::_SMERP_AUTH_MECH_SASL;
+#endif
+#ifdef WITH_GSSAPI
+	else if( s == "GSSAPI" )	return Mech::_SMERP_AUTH_MECH_GSSAPI;
+#endif
+	else				return Mech::_SMERP_AUTH_MECH_UNDEFINED;
+}
+
+} // namespace Authentication
+} // namespace _SMERP
--- src/authentication.hpp
+++ src/authentication.hpp
@@ -5,8 +5,53 @@
 #ifndef _AUTHENTICATION_HPP_INCLUDED
 #define _AUTHENTICATION_HPP_INCLUDED
 
-namespace _SMERP {
+#include <string>
 
+namespace _SMERP {
+	namespace Authentication {
+
+class Mech {
+public:
+	enum AuthMech {
+#ifdef _WIN32
+		_SMERP_AUTH_MECH_WINAD,			// Windows AD
+		_SMERP_AUTH_MECH_WINKERB,		// integrated Windows authentication
+							// (Kerberos)
+#endif
+#ifdef WITH_OPENLDAP
+		_SMERP_AUTH_MECH_LDAP,			// LDAP (Unix and Windows)
+#endif
+#ifdef WITH_PAM
+		_SMERP_AUTH_MECH_PAM,			/// Suns PAM
+#endif
+#ifdef WITH_SASL
+		_SMERP_AUTH_MECH_SASL,			/// Cyrus SASL
+#endif
+#ifdef WITH_GSSAPI
+		_SMERP_AUTH_MECH_GSSAPI,		/// GSSAPI
+#endif
+		_SMERP_AUTH_MECH_TEXT_FILE,		/// simple textfile
+		_SMERP_AUTH_MECH_UNDEFINED
+	};
+
+	static AuthMech str2AuthMech( const std::string s );
+};
+
+// map enum values to strings
+template< typename CharT, typename TraitsT >
+inline std::basic_ostream< CharT, TraitsT > &operator<< ( std::basic_ostream< CharT, TraitsT >& s, Mech::AuthMech f )
+{
+	static const CharT *const str[] = {
+		"WINAD", "WINKERB", "LDAP", "PAM", "SASL", "GSSAPI", "TEXT_FILE" };
+	if( static_cast< size_t >( f ) < ( sizeof( str ) / sizeof( *str ) ) ) {
+		s << str[f];
+	} else {
+		s << "Unknown enum used '" << static_cast< int >( f ) << "'";
+	}
+	return s;
+}
+
+} // namespace Authentication
 } // namespace _SMERP
 
 #endif // _AUTHENTICATION_HPP_INCLUDED
