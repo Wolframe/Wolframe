9e8c8d8a18210540dee6901558a93faed3802148
Andreas Baumann <abaumann@yahoo.com>
2011-02-20 11:20:46 +0100

    removed authentication methods enum

--- src/authentication.cpp
+++ src/authentication.cpp
@@ -7,28 +7,6 @@
 namespace _SMERP {
 	namespace Authentication {
 
-Mech::AuthMech str2AuthMech( const std::string s )
-{
-	if( s == "TEXT_FILE" )		return Mech::_SMERP_AUTH_MECH_TEXT_FILE;
-#ifdef _WIN32
-	else if( s == "WINAD" )		return Mech::_SMERP_AUTH_MECH_WINAD;
-	else if( s == "WINKERB" )	return Mech::_SMERP_AUTH_MECH_WINKERB;
-#endif
-#ifdef WITH_OPENLDAP
-	else if( s == "LDAP" )		return Mech::_SMERP_AUTH_MECH_LDAP;
-#endif
-#ifdef WITH_PAM
-	else if( s == "PAM" )		return Mech::_SMERP_AUTH_MECH_PAM;
-#endif
-#ifdef WITH_SASL
-	else if( s == "SASL" )		return Mech::_SMERP_AUTH_MECH_SASL;
-#endif
-#ifdef WITH_GSSAPI
-	else if( s == "GSSAPI" )	return Mech::_SMERP_AUTH_MECH_GSSAPI;
-#endif
-	else				return Mech::_SMERP_AUTH_MECH_UNDEFINED;
-}
-
 AuthenticatorFactory::AuthenticatorFactory( )
 {
 }
@@ -64,25 +42,9 @@ Authenticator* AuthenticatorFactory::getAuthenticator( const std::string method 
 std::vector<std::string> AuthenticatorFactory::getAvailableMechs( )
 {
 	std::vector<std::string> mechs;
-		
-	mechs.push_back( "TEXT_FILE");
-#ifdef _WIN32
-	mechs.push_back( "WINAD" );
-	mechs.push_back( "WINKERB" );
-#endif
-#ifdef WITH_OPENLDAP
-	mechs.push_back( "LDAP" );
-#endif
-#ifdef WITH_PAM
-	mechs.push_back( "PAM" );
-#endif
-#ifdef WITH_SASL
-	mechs.push_back( "SASL" );
-#endif
-#ifdef WITH_GSSAPI
-	mechs.push_back( "GSSAPI" );
-#endif
-	
+	std::map<std::string, Authenticator *>::const_iterator it;
+	for( it = m_authenticators.begin( ); it != m_authenticators.end( ); it++ )
+		mechs.push_back( it->first );
 	return mechs;
 }
 
--- src/authentication.hpp
+++ src/authentication.hpp
@@ -17,35 +17,6 @@
 namespace _SMERP {
 	namespace Authentication {
 
-// the list of available authentication methods
-// (TODO: make this dynamic and in modules)
-class Mech {
-public:
-	enum AuthMech {
-#ifdef _WIN32
-		_SMERP_AUTH_MECH_WINAD,			/// Windows AD
-		_SMERP_AUTH_MECH_WINKERB,		/// integrated Windows authentication
-							/// (Kerberos)
-#endif
-#ifdef WITH_OPENLDAP
-		_SMERP_AUTH_MECH_LDAP,			/// LDAP (Unix and Windows)
-#endif
-#ifdef WITH_PAM
-		_SMERP_AUTH_MECH_PAM,			/// Suns PAM
-#endif
-#ifdef WITH_SASL
-		_SMERP_AUTH_MECH_SASL,			/// Cyrus SASL
-#endif
-#ifdef WITH_GSSAPI
-		_SMERP_AUTH_MECH_GSSAPI,		/// GSSAPI
-#endif
-		_SMERP_AUTH_MECH_TEXT_FILE,		/// simple textfile
-		_SMERP_AUTH_MECH_UNDEFINED
-	};
-
-	static AuthMech str2AuthMech( const std::string s );
-};
-
 // the basic steps the authenticator can be in
 class Step {
 public:
@@ -139,20 +110,6 @@ template<class T> T findprop( AuthenticatorFactory::properties _props, const std
 		return boost::any_cast<T>( it->value );
 	}
 	throw std::logic_error( "bad argument in property set" );
-}
-
-// map authentication method enum values to strings
-template< typename CharT, typename TraitsT >
-inline std::basic_ostream< CharT, TraitsT > &operator<< ( std::basic_ostream< CharT, TraitsT >& s, Mech::AuthMech f )
-{
-	static const CharT *const str[] = {
-		"WINAD", "WINKERB", "LDAP", "PAM", "SASL", "GSSAPI", "TEXT_FILE" };
-	if( static_cast< size_t >( f ) < ( sizeof( str ) / sizeof( *str ) ) ) {
-		s << str[f];
-	} else {
-		s << "Unknown enum used '" << static_cast< int >( f ) << "'";
-	}
-	return s;
 }
 
 } // namespace Authentication
