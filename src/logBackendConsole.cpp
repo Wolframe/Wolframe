f49f0ac77d5f163eacd58900ddbae1c4e68b238a
Andreas Baumann <abaumann@yahoo.com>
2011-03-26 16:50:07 +0100

    -

---
+++ src/logBackendConsole.cpp
@@ -0,0 +1,79 @@
+/************************************************************************
+
+ Copyright (C) 2011 Project Wolframe.
+ All rights reserved.
+
+ This file is part of Project Wolframe.
+
+ Commercial Usage
+    Licensees holding valid Project Wolframe Commercial licenses may
+    use this file in accordance with the Project Wolframe
+    Commercial License Agreement provided with the Software or,
+    alternatively, in accordance with the terms contained
+    in a written agreement between the licensee and Project Wolframe.
+
+ GNU General Public License Usage
+    Alternatively, you can redistribute this file and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wolframe is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Wolframe.  If not, see <http://www.gnu.org/licenses/>.
+
+ If you have questions regarding the use of this file, please contact
+ Project Wolframe.
+
+************************************************************************/
+
+///
+/// \file logBackendConsole.cpp
+/// \brief implementation of the console backend
+///
+
+#include <iostream>
+
+//#include "logger.hpp"
+#include "logBackendImpl.hpp"
+#include "logBackendConsole.hpp"
+
+#include "unused.h"
+
+namespace _Wolframe {
+	namespace Logging {
+
+ConsoleLogBackend::ConsoleLogBackend( )
+{
+	logLevel_ = LogLevel::LOGLEVEL_ERROR;
+}
+
+ConsoleLogBackend::~ConsoleLogBackend( )
+{
+	// nothing to do here
+}
+
+void ConsoleLogBackend::setLevel( const LogLevel::Level level )
+{
+	logLevel_ = level;
+}
+
+void ConsoleLogBackend::log( WOLFRAME_UNUSED const LogComponent component, const LogLevel::Level level, const std::string& msg )
+{
+	if ( level >= logLevel_ ) {
+		std::cerr << level << ": " << msg << std::endl;
+		std::cerr.flush( );
+	}
+}
+
+void ConsoleLogBackend::reopen( )
+{
+	// nothing to do here
+}
+
+	} // namespace Logging
+} // namespace _Wolframe
