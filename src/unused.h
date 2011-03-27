7fd678b989c70ee9147ec53d46c312862bf04a88
Andreas Baumann <abaumann@yahoo.com>
2011-03-27 12:14:04 +0200

    -

--- src/unused.h
+++ src/unused.h
@@ -7,6 +7,10 @@
 #ifndef __WOLFRAME_UNUSED_H
 #define __WOLFRAME_UNUSED_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * @brief Macro to avoid unused parameter messages in functions
  *
@@ -16,6 +20,10 @@
 #define WOLFRAME_UNUSED __attribute__((unused))
 #else
 #define WOLFRAME_UNUSED
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* ifndef __WOLFRAME_UNUSED_H */
