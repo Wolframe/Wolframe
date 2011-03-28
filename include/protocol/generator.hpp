2d4af70f232787f9156ff6904062b1537f8c0d75
Patrick Frey <patrickpfrey@yahoo.com>
2011-03-28 01:57:40 -0700

    Edited include/protocol/generator.hpp via GitHub

--- include/protocol/generator.hpp
+++ include/protocol/generator.hpp
@@ -39,40 +39,46 @@ Project Wolframe.
 namespace _Wolframe {
 namespace protocol {
 
-///! \class Generator
-/// \brief closure class with iterator function for iterating on protocol input content elements
+/// \class Generator
+/// \brief closure class with iterator function for iterating on network input content elements
 struct Generator
 {
 	typedef std::size_t size_type;
 
+	/// \enum State
+	/// \brief state of the generator used in the application processor iterating loop to decide wheter to yield execution or not.
 	enum State
 	{
-		Open,		//serving data
-		EndOfMessage,	//EWOULDBLK -> have to yield
-		Error		//an error occurred
+		Open,		///< serving data - normal input processing
+		EndOfMessage,	///< have to yield processing because end of message reached
+		Error		///< have to yield with error and stop processing with an error 
 	};
-
+	/// \enum ElementType
+	/// \brief content element type that describes the role of the element in the structured input
 	enum ElementType
 	{
-		OpenTag,	//Open new hierarchy level
-		Attribute,	//Attribute Name
-		Value,		//Content or attribute Value
-		CloseTag	//Close current hierarchy level
+		OpenTag,	///< open new hierarchy level
+		Attribute,	///< attribute name
+		Value,		///< content or attribute value
+		CloseTag	///< close current hierarchy level
 	};
 
-	//Get next element call
+	/// \brief get next element call
 	typedef bool (*GetNext)( Generator* this_, ElementType* type, void* buffer, size_type buffersize, size_type* bufferpos);
 
+	/// \brief get next element call as methof call
 	bool getNext( ElementType* type, void* buffer, size_type buffersize, size_type* bufferpos)
 	{
 		return m_getNext( this, type, buffer, buffersize, bufferpos);
 	}
 
+	/// \brief return the current state
 	State state() const
 	{
 		return m_state;
 	}
 
+	/// \brief feed the closure with network input
 	void protocolInput( void* data, size_type datasize, bool eoD)
 	{
 		m_gotEoD = eoD;
@@ -81,6 +87,7 @@ struct Generator
 		m_pos = 0;
 	}
 
+	/// \brief assignement
 	Generator& operator = (const Generator& o)
 	{
 		m_ptr = o.m_ptr;
@@ -92,23 +99,30 @@ struct Generator
 		return *this;
 	}
 
+	/// \brief constructor
 	Generator( const GetNext& gn) :m_ptr(0),m_pos(0),m_size(0),m_gotEoD(false),m_state(Open),m_errorCode(0),m_getNext(gn){}
 
+	/// \brief get error code in case of error state
 	int getError() const				{return m_errorCode;}
+	/// \brief get end of data flag passed to generator. Tells if more network input has to be passed to the application processor
 	bool gotEoD() const				{return m_gotEoD;}
+	/// \brief get data at current iterator cursor position
 	void* ptr() const				{return(void*)((char*)m_ptr+m_pos);}
+	/// \brief get rest of data from the current iterator cursor position
 	size_type size() const			{return (m_pos<m_size)?(m_size-m_pos):0;}
+	/// \brief skip current iterator cursor position n bytes forward
 	void skip( size_type n)			{if ((m_pos+n)>=m_size) m_pos=m_size; else m_pos+=n;}
+	/// \brief set generator state with error code
 	void setState( State s, int e=0)		{m_state=s;m_errorCode=e;}
 
 private:
-	void* m_ptr;
-	size_type m_pos;
-	size_type m_size;
-	bool m_gotEoD;
-	State m_state;
-	int m_errorCode;
-	GetNext m_getNext;
+	void* m_ptr;		///< pointer to network input buffer
+	size_type m_pos;		///< current iterator cursor position
+	size_type m_size;		///< size of network input buffer
+	bool m_gotEoD;		///< got end of data flag
+	State m_state;		///< generator state
+	int m_errorCode;		///< error code
+	GetNext m_getNext;		///< get next method pointer
 };
 
 }}//namespace
