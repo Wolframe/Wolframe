30cc387414da390255af2be4794baea1ef5c5103
Andreas Baumann <abaumann@yahoo.com>
2011-04-10 20:30:40 +0200

    added amalgamation of sqlite3 and building on Windows, cant test currently

--- makefiles/nmake/help.mk
+++ makefiles/nmake/help.mk
@@ -13,6 +13,7 @@ Available optional features:
 WITH_SSL=1             use OpenSSL additionally for communication encryption
 WITH_LUA=1             build code depending on Lua
 WITH_QT=1              build code depending on Qt
+WITH_SQLITE3=1         build AAAA and DB connectors for Sqlite3
 
 Avaliable optional features during testing only:
 
@@ -20,4 +21,4 @@ WITH_GTEST=1           use Google Unit testing library
 WITH_EXPECT=1          use Expect/Tcl for system testing
 
 Example:
-nmake /nologo /f Makefile.W32 WITH_SSL=1 WITH_GTEST=1 WITH_EXPECT=1 WITH_LUA=1 WITH_QT=1
+nmake /nologo /f Makefile.W32 WITH_SSL=1 WITH_GTEST=1 WITH_EXPECT=1 WITH_LUA=1 WITH_QT=1 WITH_SQLITE3=1
