95f17170d8d9b1317e57a4574d44fb6ce70424d6
Patrick Frey <patrickpfrey@yahoo.com>
2011-04-18 18:30:23 +0200

    fix broken trunk. grmpf

--- include/base64.hpp
+++ include/base64.hpp
@@ -1,7 +1,7 @@
-namespace _Wolframe {
-	namespace base64 {
-
 #include <string>
+
+namespace _Wolframe {
+namespace base64 {
 
 std::string base64_encode( unsigned char const*, size_t len);
 std::string base64_decode( std::string const& s );
