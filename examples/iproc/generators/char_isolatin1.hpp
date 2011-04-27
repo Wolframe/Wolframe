2c0daceba8dd93fa106b2f5f4e9ecd46805e10f1
Patrick Frey <patrickpfrey@yahoo.com>
2011-04-27 07:02:02 -0700

    temporary checkin of things that do not belong here

--- examples/iproc/generators/char_isolatin1.hpp
+++ examples/iproc/generators/char_isolatin1.hpp
@@ -4,8 +4,103 @@
 #include "protocol/formatoutput.hpp"
 #include <cstring>
 
+#if 0 //only temporarily here
+#include "textwolf.hpp"
+#endif
+
+
 namespace _Wolframe {
 namespace filter {
+
+#if 0 //only temporarily here
+template <struct Charset_>
+struct XmlFilterT
+{
+	struct Generator //[+]:public protocol::Generator
+	{
+		enum ErrorCodes {Ok=0,ErrBufferTooSmall=1,ErrBrokenInputStream=2};
+
+		struct InputIterator
+		{
+			struct EOM{};
+			Generator* m_gen;
+
+			InputIterator() :m_gen(0) {}
+			InputIterator( Generator* gen) :m_gem(gen){}
+			InputIterator( const InputIterator& o) :m_gem(o.m_gen){}
+
+			char operator*()
+			{
+				if (m_gen->m_pos >= m_gen->m_size) throw EOM();
+				return ((char*)m_gen->m_ptr)[ m_gen->m_pos];
+			}
+			InputIterator& operator++()
+			{
+				m_gen->m_pos++;
+			}
+		};
+		typedef textwolf::XMLScanner<InputIterator,Charset_,charset::UTF8> XMLScanner;
+		char m_outputbuf;
+		InputIterator itr;
+		XMLScanner* scanner;
+
+		Generator() :scanner(0)
+		{
+			itr = InputIterator(this);
+			scanner = new XMLScanner( itr, &m_outputbuf, 1);
+		}
+
+		~Generator()
+		{
+			delete scanner;
+		}
+
+		virtual bool getNext( ElementType* type, void* buffer, size_type buffersize, size_type* bufferpos)
+		{
+			scanner->setOutputBuffer( (char*)buffer + *bufferpos, buffersize - *bufferpos);
+			try
+			{
+				itr++;
+				*bufferpos += itr->size();
+				switch (itr->type())
+				{
+					case ... *type = ... break;
+					case ... *type = ... break;
+					case EOF
+                                        case Error: setState( Error, errorcode);
+				}
+			}
+			catch (InputIterator::EOM)
+			{
+				if (gotEoD())
+				{
+					setState( Error, ErrBrokenInputStream);
+					return false;
+				}
+				else
+				{
+					setState( EndOfMessage);
+					return false;
+				}
+			};
+			if (EOF)
+			{
+				if (gotEoD())
+				{
+					setState( Open);
+					return false;
+				}
+				else
+				{
+					setState( EndOfMessage);
+					return false;
+				}
+			}
+			return true;
+		}
+	};
+};
+#endif
 
 struct CharIsoLatin1
 {
