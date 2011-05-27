6a2fe118831251bd96a3d3ce626cc954d8727174
Patrick Frey <patrickpfrey@yahoo.com>
2011-05-27 02:05:57 -0700

    documentation

--- experimental/include/config/traits.hpp
+++ experimental/include/config/traits.hpp
@@ -30,7 +30,7 @@ Project Wolframe.
 
 ************************************************************************/
 /// \file config/traits.hpp
-/// \brief defines the tags for dispatching parse functions for disjunct type sets
+/// \brief defines the traits for the type categories for dispatching parse functions deoending on the value type
 
 #ifndef _Wolframe_CONFIG_TRAITS_HPP_INCLUDED
 #define _Wolframe_CONFIG_TRAITS_HPP_INCLUDED
@@ -44,9 +44,9 @@ namespace _Wolframe {
 namespace config {
 namespace traits
 {
-struct struct_ {};	///< the tag marks a type that is a structure with named elements
-struct vector_ {};	///< the tag marks a type that std::vector of any type
-struct atom_ {};	///< the tag marks a type is convertible from a string through boost::lexical_cast
+struct struct_ {};	///< category tag for a structure with named elements
+struct vector_ {};	///< category tag for a std::vector of any type
+struct atom_ {};	///< category tag for a type that is convertible from a string through boost::lexical_cast
 
 /// \brief conditional template for detecting if a type is a class with a static/member method description() returning a const pointer to a structure description as defined in config/descriptionBase.hpp
 /// see http://drdobbs.com/article/print?articleId=227500449&siteSectionName= "Checking Concept Without Concepts in C++"
@@ -79,7 +79,7 @@ struct has_description_method<T,true>:
 {};
 
 
-/// trait tag vector_
+/// \brief get category vector_ for a type
 /// returns vector_ if  std::vector<T::value_type> EQUALS T. This is true, when T is a std::vector of any kind
 template <typename T>
 typename boost::enable_if_c<
@@ -87,15 +87,15 @@ typename boost::enable_if_c<
 	,vector_>::type getCategory( const T&) { return vector_();}
 
 
-/// trait tag struct_
+/// \brief get category struct_ for a type
 /// returns struct_ if T fulfills has a method description with no params returning a const pointer to a config::DescriptionBase
 template <typename T>
 typename boost::enable_if_c<
-		has_description_method<T>::value
-		,struct_>::type getCategory( const T&) { return struct_();}
+	has_description_method<T>::value
+	,struct_>::type getCategory( const T&) { return struct_();}
 
 
-/// trait tag atom_
+/// \brief get category atom_ for a type
 /// returns atom_ if T T fulfills the is_arithmetic condition or is a string
 template <typename T>
 typename boost::enable_if_c<
