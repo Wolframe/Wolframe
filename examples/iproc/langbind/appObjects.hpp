0973227c1fd5efa113694d52c7ed2513650f294b
Patrick Frey <patrickpfrey@yahoo.com>
2011-06-08 05:37:30 -0700

    added optional buffersize parameter to create filter methods. Some filters need additional memory. Depends on the filter.

--- examples/iproc/langbind/appObjects.hpp
+++ examples/iproc/langbind/appObjects.hpp
@@ -91,11 +91,11 @@ struct System
 	/// \brief create a new input filter function
 	/// \param[in] name name of the filter or the default filter if not specified
 	/// \param[in] buffersize size of buffer in bytes to use
-	virtual protocol::InputFilter* createInputFilter( const char* name=0) const;
+	virtual protocol::InputFilter* createInputFilter( const char* name=0, unsigned int buffersize=0) const;
 	/// \brief create a new format output filter function
 	/// \param[in] name name of the filter or the default filter if not specified
 	/// \param[in] buffersize size of buffer in bytes to use
-	virtual protocol::FormatOutput* createFormatOutput( const char* name=0) const;
+	virtual protocol::FormatOutput* createFormatOutput( const char* name=0, unsigned int buffersize=0) const;
 };
 
 /// \class Input
