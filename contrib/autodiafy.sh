fe3cb0091ce7d859a411a0d8d967d248c0f30b69
Andreas Baumann <abaumann@yahoo.com>
2011-06-09 17:49:32 +0200

    -

--- contrib/autodiafy.sh
+++ contrib/autodiafy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-AUTODIA='autodia.pl -l c++'
+AUTODIA='autodia -l c++'
 $AUTODIA -z -o database.png -i \
 	"include/database/DBreference.hpp include/database/PostgreSQL.hpp include/database/SQLite.hpp include/database/database.hpp"
+
+$AUTODIA -z -o logger.png -i \
+	"include/singleton.hpp include/logger.hpp include/logger/logError.hpp include/logger/logSyslogFacility.hpp include/logger/logBackend.hpp include/logger/logComponent.hpp include/logger/logLevel.hpp"
