55b2be389aa7d7ecaca7924445293c21436d0126
Patrick Frey <patrickpfrey@yahoo.com>
2011-06-09 02:18:59 -0700

    fixed bugs in config traits

--- include/config/structPointer.hpp
+++ include/config/structPointer.hpp
@@ -38,25 +38,24 @@ Project Wolframe.
 #include <vector>
 #include <cstring>
 #include <iostream>
+#include <stdexcept>
 #include "config/traits.hpp"
 #include "logger/logLevel.hpp"
 
 namespace _Wolframe {
 namespace config {
 
-#if 0
 /// \brief returns true, if the structure T matches to name
 template <typename T>
-static typename boost::enable_if_c<boost::is_same<Category<T>,struct_>::value,bool>::type
-matchesElement_( const char* name, const T& value)
+bool matchesElement_( const char* name, const T* value, const traits::struct_&)
 {
-	const DescriptionBase* dd = value.description();
+	const DescriptionBase* dd = value->description();
 	std::vector<DescriptionBase::Item>::const_iterator itr,end;
-	const char* vv = (const char*)(const void*)(&value);
+	const char* vv = (const char*)(const void*)(value);
 
 	for (itr=dd->m_ar.begin(),end=dd->m_ar.end(); itr != end; ++itr)
 	{
-		if (strcmp( itr->m_type.c_str(), "std::string") == 0)
+		if (strcmp( itr->m_type, "std::string") == 0)
 		{
 			const std::string* pp = (const std::string*)(const void*)(vv + itr->m_ofs);
 			return (strcmp( name, pp->c_str()) == 0);
@@ -66,39 +65,37 @@ matchesElement_( const char* name, const T& value)
 }
 
 template <typename T>
-static typename boost::disable_if_c<boost::is_same<Category<T>,struct_>::value,bool>::type
-matchesElement_( const char* , const T&)
+bool matchesElement_( const char* , const T*, const traits::nonstruct_&)
 {
 	return false;
 }
 
 /// \brief findElement_ for struct_
 template <typename T>
-static typename boost::enable_if_c<boost::is_same<Category<T>,struct_>::value,const void*>::type
-findElement_( const char* type, const char* name, const T& value)
+const void* findElement_( const char* type, const char* name, const T* value, const traits::struct_&)
 {
-	const DescriptionBase* dd = value.description();
+	const DescriptionBase* dd = value->description();
 	std::vector<DescriptionBase::Item>::const_iterator itr,end;
-	const char* vv = (const char*)(const void*)(&value);
+	const char* vv = (const char*)(const void*)(value);
 	const void* rt = 0;
 
 	for (itr=dd->m_ar.begin(),end=dd->m_ar.end(); itr != end; ++itr)
 	{
 		const void* ee = (const void*)(vv + itr->m_ofs);
-		if (strcmp( itr->m_type.c_str(), type) == 0)
+		if (strcmp( itr->m_type, type) == 0)
 		{
 			if (strcmp( itr->m_name.c_str(), name) == 0 || itr->m_matches( name, ee))
 			{
-				if (rt) throw std::logical_error( "ambiguus reference");
+				if (rt) throw std::logic_error( "ambiguus reference");
 				rt = ee;
 			}
 		}
 		else
 		{
-			const void* rr = itr->m_find( type, name, ee);
+			const void* rr = itr->m_find( type, name);
 			if (rr)
 			{
-				if (rt) throw std::logical_error( "ambiguus reference");
+				if (rt) throw std::logic_error( "ambiguus reference");
 				rt = rr;
 			}
 		}
@@ -106,21 +103,20 @@ findElement_( const char* type, const char* name, const T& value)
 	return rt;
 }
 
-/// \brief findElement_ for vector_ of struct_
+/// \brief findElement_ for vector_
 template <typename T>
-static typename boost::enable_if_c<boost::is_same<Category<T>,vector_>::value && boost::is_same<Category<typename T::value_type>,struct_>::value,const void*>::type
-findElement_( const char* type, const char* name, const T& value)
+const void* findElement_( const char* type, const char* name, const T* value, const traits::vector_&)
 {
 	const void* rt = 0;
 	const char* elemtype = typeid( typename T::value_type).name();
 	if (strcmp( type, elemtype) == 0)
 	{
 		T::const_iterator itr,end;
-		for (itr=value.begin(),end=value.end(); itr!=end; ++itr)
+		for (itr=value->begin(),end=value->end(); itr!=end; ++itr)
 		{
-			if (matchesElement_( name, &*itr))
+			if (matchesElement_( name, &*itr, traits::getCategory( *itr)))
 			{
-				if (rt) throw std::logical_error( "ambiguus reference");
+				if (rt) throw std::logic_error( "ambiguus reference");
 				rt = (const void*)&*itr;
 			}
 		}
@@ -128,12 +124,12 @@ findElement_( const char* type, const char* name, const T& value)
 	else
 	{
 		T::const_iterator itr,end;
-		for (itr=value.begin(),end=value.end(); itr!=end; ++itr)
+		for (itr=value->begin(),end=value->end(); itr!=end; ++itr)
 		{
-			const void* rr = findElement_( type, name, &*itr);
+			const void* rr = findElement_( type, name, &*itr, traits::getCategory( *itr));
 			if (rr)
 			{
-				if (rt) throw std::logical_error( "ambiguus reference");
+				if (rt) throw std::logic_error( "ambiguus reference");
 				rt = rr;
 			}
 		}
@@ -141,37 +137,33 @@ findElement_( const char* type, const char* name, const T& value)
 	return rt;
 }
 
-/// \brief findElement_ for non struct_ and non vector_ of struct_
+/// \brief findElement_ for atom_
 template <typename T>
-static typename boost::disable_if_c<
-			boost::is_same<Category<T>,struct_>::value
-			|| (boost::is_same<Category<T>,vector_>::value && boost::is_same<Category<typename T::value_type>,struct_>::value)
-		,const void*>::type
-findElement_( const char*, const char*, const T&)
+const void* findElement_( const char*, const char*, const T*, const traits::atom_&)
 {
-	return false;
+	return 0;
 }
-#endif
+
+/// \brief findElement_ for atom_
+template <typename T>
+const void* findElement_( const char*, const char*, const T*, const traits::foreign_&)
+{
+	return 0;
+}
 
 template <typename Element>
 struct ElementPointer
 {
-//[-]	static bool matchesElement( const char* name, const void* value)
-	static bool matchesElement( const char* , const void*)
+	static bool matchesElement( const char* name, const void* value)
 	{
-		return true;
-//[-]		return matchesElement_( name, *(const Element*)value);
+		return matchesElement_( name, (const Element*)value, traits::getCategory( *(const Element*)value));
 	}
-//[-]	static const void* findElement( const char* name, const void* value)
-	static const void* findElement( const char* , const void* )
+	static const void* findElement( const char* name, const void* value)
 	{
-//[-]		const Element* ee = *(const Element*)value;
-//[-]		const char* type = typeid(Element).name();
-//[-]		return findElement_( type, name, ee);
-		return 0;
+		const char* type = typeid(Element).name();
+		return findElement_( type, name, (const Element*)value, traits::getCategory( *(const Element*)value));
 	}
 };
 
 }}// end namespace
-#endif
 
