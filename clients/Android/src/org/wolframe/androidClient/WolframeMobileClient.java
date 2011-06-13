604fdab8cb41ea099e97b2414ed4ceb15f69eb96
Mihai Barbos <mihai.barbos@gmail.com>
2011-06-13 13:45:21 +0200

    added an empty android project

---
+++ clients/Android/src/org/wolframe/androidClient/WolframeMobileClient.java
@@ -0,0 +1,15 @@
+package org.wolframe.androidClient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WolframeMobileClient extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
