97b809723189366f41b6f9dbff15c8218248e96e
Andreas Baumann <abaumann@yahoo.com>
2011-06-18 12:07:10 +0200

    -

--- contrib/osc/getall.sh
+++ contrib/osc/getall.sh
@@ -7,9 +7,15 @@ fi
 for r in `cat repo.cache | tr -s ' ' '@'`; do
 	repo=`echo $r | cut -f 1 -d@`
 	arch=`echo $r | cut -f 2 -d@`
+	mkdir -p packages/$repo/$arch
+	
 	echo "Getting packages for $repo, $arch"
 	osc getbinaries $repo $arch
 	
 	echo "Getting buildlog for $repo, $arch"
 	osc buildlog $repo $arch > buildlog-$repo-$arch.log	
+	
+	mv *.log packages/$repo/$arch
+	mv *.rpm packages/$repo/$arch
+	mv *.deb packages/$repo/$arch
 done
