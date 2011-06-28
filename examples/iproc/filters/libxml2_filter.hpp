3dcee8caa36df1b937fa442f3e984a587117d351
Patrick Frey <patrickpfrey@yahoo.com>
2011-06-28 07:56:32 -0700

    started with libxml2 instead of libxml2++

--- examples/iproc/filters/libxml2_filter.hpp
+++ examples/iproc/filters/libxml2_filter.hpp
@@ -33,12 +33,15 @@ Project Wolframe.
 ///\brief Filter using the libxml2 library for input and output
 #include "bufferingFilterBase.hpp"
 #include <cstddef>
+#include <vector>
+#include <string>
+#include <libxml/tree.h>
 #include <libxml++/libxml++.h>
 #include <libxml++/parsers/textreader.h>
 
 namespace _Wolframe {
 namespace filter {
-namespace libxml2 {
+namespace libxml2pp {
 
 class Container
 {
@@ -177,7 +180,162 @@ private:
 };
 }//namespace
 
+typedef BufferingInputFilter<libxml2pp::Container,std::string> Libxml2ppInputFilter;
+
+
+
+
+namespace libxml2 {
+
+class Container
+{
+public:
+	///\brief Enumeration of states of the libxml2 parser
+	enum State
+	{
+		Init,		///< started
+		Tag,		///< reading tag
+		Attribute,	///< reading attribute name
+		AttributeValue,	///< reading attribute value
+		EndOfData	///< reached end of data
+	};
+
+	struct Element
+	{
+		std::string& value() const			{return m_value;}
+		protocol::InputFilter::ElementType type() const {return m_type;}
+	private:
+		friend class Container;
+	};
+
+	Container( void* src, std::size_t srcsize)
+		:m_doc(0),m_state(Init),m_type(protocol::InputFilter::Value),m_depth(0)
+	{
+		m_doc = xmlReadMemory( buffer, size, 0/*URL*/, 0/*encoding*/, 0/*options*/); 
+		if (m_doc != 0)
+		{
+			if (doc->children)
+			{
+				nodestk.push_back( doc->children);
+			}
+		}
+	}
+
+	void skip()
+	{
+		m_element.m_value.clear();
+		for (;;)
+		{
+			while (m_depth > m_reader.get_depth())
+			{
+				m_element.m_type = protocol::InputFilter::CloseTag;
+				return;
+			}
+
+			switch (m_state)
+			{
+				case Init:
+					if (m_reader.read())
+					{
+						m_state = Tag;
+						m_element.m_value.append( m_reader.get_name());
+						m_element.m_type = protocol::InputFilter::OpenTag;
+						return;
+					}
+					else
+					{
+						m_element.m_type = protocol::InputFilter::CloseTag;
+						m_state = EndOfData;
+						return;
+					}
+
+				case Tag:
+					if (m_reader.has_attributes())
+					{
+						m_reader.move_to_first_attribute();
+						m_element.m_value.append( m_reader.get_name());
+						m_element.m_type = protocol::InputFilter::Attribute;
+						m_state = AttributeValue;
+						return;
+					}
+					else if (reader.has_value())
+					{
+						m_element.m_value.append( m_reader.reader.get_value());
+						m_element.m_type = protocol::InputFilter::Value;
+						m_state = Init;
+						return;
+					}
+					else
+					{
+						m_state = Init;
+						continue;
+					}
+
+				case Attribute:
+					if (reader.move_to_next_attribute())
+					{
+						m_element.m_value.append( m_reader.get_name());
+						m_element.m_type = protocol::InputFilter::Attribute;
+						m_state = AttributeValue;
+						return;
+					}
+					else if (reader.has_value())
+					{
+						m_element.m_value.append( m_reader.reader.get_value());
+						m_element.m_type = protocol::InputFilter::Value;
+						m_state = Init;
+						return;
+					}
+
+				case AttributeValue:
+					m_element.m_value.append( m_reader.reader.get_value());
+					m_element.m_type = protocol::InputFilter::Value;
+					m_state = Attribute;
+					return;
+
+				case EndOfData:
+					m_element.m_type = protocol::InputFilter::CloseTag;
+					return;
+
+			}
+		}
+	}
+
+
+	class iterator
+	{
+		iterator( Container* c) 	:m_container(c){}
+		iterator()			:m_container(0){}
+
+		iterator& operator++()
+		{
+			skip();
+			return *this;
+		}
+
+		const Element& operator*() const
+		{
+			return m_container->m_element;
+		}
+
+		const Element* operator->() const
+		{
+			return &m_container->m_element;
+		}
+	private:
+		Container* m_container;
+	};
+private:
+	xmlDocPtr m_doc;
+	State m_state;
+	Element m_element;
+	protocol::InputFilter::ElementType m_type;
+	std::vector<xmlNodePtr> m_nodestk;
+};
+}//namespace
+
 typedef BufferingInputFilter<libxml2::Container,std::string> Libxml2InputFilter;
+
 
 }}//namespace
 #endif
