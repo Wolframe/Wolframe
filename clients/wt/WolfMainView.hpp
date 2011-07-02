2ad7557ea4e58ec24fa808e0c719ec54a9c0de9b
Andreas Baumann <abaumann@yahoo.com>
2011-07-02 22:25:46 +0200

    fixed Wt menu, started to add abot dialogs, playing with ExtJS

--- clients/wt/WolfMainView.hpp
+++ clients/wt/WolfMainView.hpp
@@ -35,12 +35,18 @@
 #define _WOLF_MAIN_VIEW
 
 #include "Wt/WContainerWidget"
-#include "Wt/WMenu"
-#include "Wt/WStackedWidget"
+#include "Wt/WImage"
 #include "Wt/WText"
-#include "Wt/WSubMenuItem"
+#include "Wt/WGridLayout"
+
+#include "Wt/Ext/Menu"
+#include "Wt/Ext/ToolBar"
+#include "Wt/Ext/MessageBox"
+#include "Wt/Ext/Dialog"
+#include "Wt/Ext/Container"
 
 using namespace Wt;
+using namespace Wt::Ext;
 
 namespace _Wolframe {
 	namespace WtClient {
@@ -50,20 +56,71 @@ class WolfMainView : public WContainerWidget
 	public:
 		WolfMainView( WContainerWidget *_parent ) : WContainerWidget( _parent )
 		{
-// create the stack where the contents will be located
- Wt::WStackedWidget *contents = new Wt::WStackedWidget(this);
+			ToolBar *toolBar = new ToolBar( this );
+			Menu *menu = new Menu( );
+			MenuItem *item;
+			
+			toolBar->addButton( "File", menu );
+			menu->addItem( "Exit" );
 
- // create a menu
- Wt::WMenu *menu = new Wt::WMenu(contents, Wt::Horizontal, this);
- menu->setRenderAsList(true);
- menu->setStyleClass( WString("menu" ));
+			menu = new Menu( );
+			toolBar->addButton( "Forms", menu );
+			menu->addItem( "form1.ui" );
+			menu->addItem( "form2.ui" );
+			
+			menu = new Menu( );
+			toolBar->addButton( "Themes", menu );
+			item = menu->addItem( "default" );
+			item = menu->addItem( "gray" );
+			item = menu->addItem( "vista" );
 
- // add four items using the default lazy loading policy.
- menu->addItem("Introduction", new Wt::WText("intro"));
- menu->addItem("Download", new Wt::WText("Not yet available"));
- menu->addItem("Demo", new Wt::WText("Not yet available"));
- menu->addItem(new Wt::WMenuItem("Demo2", new Wt::WText("Not yet available")));
+			menu = new Menu( );
+			toolBar->addButton( "Help", menu );
+			item = menu->addItem( "About" );
+			item->activated( ).connect( this, &WolfMainView::showAbout );
+			menu->addSeparator( );
+			item = menu->addItem( "About Wt" );
+			item->activated( ).connect( this, &WolfMainView::showWtAbout );
+			menu->addSeparator( );
+			menu->addItem( "Debug Window" );			
+		}
+		
+		void showAbout( )
+		{
+			MessageBox::show( "wtclient", "wtclient", Ok );
+		}
 
+		void showWtAbout( )
+		{
+			Dialog dialog;
+			dialog.setWindowTitle( "About Wt" );
+			dialog.resize( 400,300 );
+
+			Button *okButton = new Button( "Ok" );
+			okButton->activated( ).connect( SLOT( &dialog, Dialog::accept ) );
+			dialog.addButton( okButton );
+			okButton->setDefault( true );
+
+			WContainerWidget *theContents = new WContainerWidget( );
+			dialog.contents( )->addWidget( theContents );
+			WGridLayout *theLayout = new WGridLayout( );
+			theContents->setLayout( theLayout );
+
+/*
+			WImage *image = new WImage( "images/emweb_powered.jpg" );
+			theContents->addWidget( image );
+			theLayout->addWidget( image, 0, 0 );
+
+			image = new WImage( "images/wt.png" );
+			theContents->addWidget( image );
+			theLayout->addWidget( image, 0, 2 );
+*/
+
+			WText *text = new WText( tr( "about.wt" ), Wt::XHTMLText );
+			theContents->addWidget( text );
+			theLayout->addWidget( text, 0, 0 );
+
+			dialog.exec( );
 		}
 };
 
