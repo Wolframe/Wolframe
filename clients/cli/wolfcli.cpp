c090326ce0c60fc90ea678580157e00ffc6a05c5
Andreas Baumann <abaumann@yahoo.com>
2011-07-06 21:33:34 +0200

    some more CLIing

--- clients/cli/wolfcli.cpp
+++ clients/cli/wolfcli.cpp
@@ -46,15 +46,94 @@ class WolfClient
 {
 	private:
 		boost::asio::ip::tcp::socket m_socket;
+		boost::asio::deadline_timer m_deadline_timer;
+		boost::asio::streambuf m_input_buffer;
 
 	public:
 		WolfClient( boost::asio::io_service& io_service )
-			: m_socket( io_service )
+			: m_socket( io_service ), m_deadline_timer( io_service )
 		{
 		}
 
 		void start( boost::asio::ip::tcp::resolver::iterator endpoint_iter )
 		{
+			start_connect( endpoint_iter );
+
+			m_deadline_timer.async_wait( boost::bind( &WolfClient::check_deadline, this ) );
+		}
+
+	private:
+		void start_connect( boost::asio::ip::tcp::resolver::iterator endpoint_iter )
+		{
+			m_deadline_timer.expires_from_now( boost::posix_time::seconds( 60 ) );
+
+			m_socket.async_connect( endpoint_iter->endpoint( ),
+				boost::bind( &WolfClient::handle_connect, this, _1, endpoint_iter ) );
+		}
+
+		void handle_connect( const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::iterator endpoint_iter )
+		{
+			if( ec ) {
+				m_socket.close( );
+				std::cerr << "Connect error: " << ec.message( ) << std::endl;
+				return;
+			}
+
+			std::cerr << "Connected to " << endpoint_iter->endpoint( ) << std::endl;
+
+			start_read( );
+			start_write( );
+		}
+
+		void start_read( )
+		{
+			m_deadline_timer.expires_from_now( boost::posix_time::seconds( 30 ) );
+			boost::asio::async_read_until( m_socket, m_input_buffer, '\n',
+				boost::bind( &WolfClient::handle_read, this, _1 ) );
+		}
+
+		void handle_read( const boost::system::error_code &ec )
+		{
+			if( ec ) {
+				std::cerr << "Error reading: " << ec.message( ) << std::endl;
+				return;
+			}
+
+			std::string line;
+			std::istream is( &m_input_buffer );
+			std::getline( is, line );
+
+			if( !line.empty( ) ) {
+				std::cout << line << std::endl;
+			}
+
+			start_read( );
+		}
+
+		void start_write( )
+		{
+			boost::asio::async_write( m_socket, boost::asio::buffer( "\n", 1 ),
+				boost::bind( &WolfClient::handle_write, this, _1 ) );
+		}
+
+		void handle_write( const boost::system::error_code &ec )
+		{
+			if( ec ) {
+				std::cerr << "Error writing: " << ec.message( ) << std::endl;
+				return;
+			}
+		}
+
+		void check_deadline( )
+		{
+			if( m_deadline_timer.expires_at( ) <= boost::asio::deadline_timer::traits_type::now( ) ) {
+				std::cerr << "Timeout on read.. terminating connection" << std::endl;
+				m_socket.close( );
+				m_deadline_timer.expires_at( boost::posix_time::pos_infin );
+				return;
+			}
+
+			m_deadline_timer.async_wait( boost::bind( &WolfClient::check_deadline, this ) );
 		}
 };
 
