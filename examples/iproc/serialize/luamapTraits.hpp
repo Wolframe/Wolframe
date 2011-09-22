e95eb617cf95af074e405a83a5cebe2e9bf89d4b
Patrick Frey <patrickpfrey@yahoo.com>
2011-09-22 10:46:12 +0300

    Edited examples/iproc/serialize/luamapTraits.hpp via GitHub

--- examples/iproc/serialize/luamapTraits.hpp
+++ examples/iproc/serialize/luamapTraits.hpp
@@ -34,4 +34,73 @@ Project Wolframe.
 
 #ifndef _Wolframe_SERIALIZE_LUAMAP_TRAITS_HPP_INCLUDED
 #define _Wolframe_SERIALIZE_LUAMAP_TRAITS_HPP_INCLUDED
+
+namespace _Wolframe {
+namespace serialize {
+
+struct struct_ {};  			///< category tag for a structure with named elements
+struct vector_ :public nonstruct_{};		///< category tag for a std::vector of any type
+struct arithmetic_ :public atom_ {};		///< category tag for a type that is convertible from a string through boost::lexical_cast
+struct bool_ :public atom_ {};			///< category tag for a boolean type
+
+///\brief conditional template for detecting if a type is a class with a static/member method getDescription() returning a const pointer to a structure description as defined in config/descriptionBase.hpp
+/// see http://drdobbs.com/article/print?articleId=227500449&siteSectionName= "Checking Concept Without Concepts in C++"
+template<typename T,bool is_class_type=boost::is_class<T>::value>
+struct has_description_method: boost::false_type {};
+
+template<typename T>
+struct has_description_method_noprm
+{
+	typedef char small_type;
+	struct large_type {small_type dummy[2];};
+
+	template<const DirectmapBase* (T::*)()> struct tester_member_signature;
+	template<const DirectmapBase* (*)()> struct tester_static_signature;
+
+	template<typename U>
+	static small_type has_matching_member(tester_member_signature<&U::getDescription>*);
+	template<typename U>
+	static small_type has_matching_member(tester_static_signature<&U::getDescription>*);
+	template<typename U>
+	static large_type has_matching_member(...);
+
+	///\brief value with the boolean property corresponding has getDescription static/member method without paramater
+	static const bool value=sizeof(has_matching_member<T>(0))==sizeof(small_type);
+};
+
+template<typename T>
+struct has_description_method<T,true>:
+	boost::integral_constant<bool, has_description_method_noprm<T>::value>
+{};
+
+
+///\brief get category vector_ for a type
+/// returns vector_ if  std::vector<T::value_type> EQUALS T. This is true, when T is a std::vector of any kind
+template <typename T>
+typename boost::enable_if_c<
+	boost::is_same< std::vector< typename T::value_type> ,T>::value && !boost::is_same<std::string,T>::value
+	,vector_>::type getCategory( const T&) { return vector_();}
+
+///\brief get category struct_ for a type
+/// returns struct_ if T has a method description with no params returning a const pointer to a config::DescriptionBase
+template <typename T>
+typename boost::enable_if_c<
+	has_description_method<T>::value
+	,struct_>::type getCategory( const T&) { return struct_();}
+
+///\brief get category arithmetic_ for a type
+/// returns arithmetic_ if T fulfills the is_arithmetic condition or is a string
+template <typename T>
+typename boost::enable_if_c<
+	(boost::is_arithmetic<T>::value && !boost::is_same<bool,T>::value) || boost::is_same<std::string,T>::value
+	,arithmetic_>::type getCategory( const T&) { return arithmetic_();}
+
+///\brief get category bool_ for a type
+/// returns bool_ if T is a bool
+template <typename T>
+typename boost::enable_if_c<
+	boost::is_same<bool,T>::value
+	,bool_>::type getCategory( const T&) { return bool_();}
+
+}}//namespace
 #endif
