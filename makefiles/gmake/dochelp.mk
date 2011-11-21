b4362e8546fc6c253da8c65c28d6ebe7420c8e4e
Andreas Baumann <abaumann@yahoo.com>
2011-11-21 17:56:33 +0100

    minifix for doc-epub format in make doc

--- makefiles/gmake/dochelp.mk
+++ makefiles/gmake/dochelp.mk
@@ -6,5 +6,6 @@ make doc-man                create man pages
 make doc-doxygen            build HTML and man version of doxygen documentation
 make doc-html               build HTML version of the manual
 make doc-pdf                build PDF version of the manual
+make doc-epub              build EPUB version of the manual
 
  
