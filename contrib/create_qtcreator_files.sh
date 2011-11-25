e3cde1c05521f749e923f89ca0b51fbe68d2857f
Mihai Barbos <mihai.barbos@gmail.com>
2011-11-25 15:30:01 +0100

    added def files for all modules

--- contrib/create_qtcreator_files.sh
+++ contrib/create_qtcreator_files.sh
@@ -5,6 +5,7 @@ find . -name '*.[ch]' -type f -print >/tmp/$$
 find . -name '*.[ch]pp' -type f -print >>/tmp/$$
 find . -name 'GNUmakefile' -type f -print >>/tmp/$$
 find . -name 'Makefile.W32' -type f -print >>/tmp/$$
+find src/modules -name '*.def' -type f -print >>/tmp/$$
 find . -name '*.conf' -type f -print >>/tmp/$$
 find . -name '*.xml' -type f -print >>/tmp/$$
 find . -name '*.rng' -type f -print >>/tmp/$$
