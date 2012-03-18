f9d57c6414d2a9b48d228d3f320567859d4d94eb
U-Andreas-PC\Andreas <abaumann@yahoo.com>
2012-03-18 13:57:23 +0100

    -

--- makefiles/nmake/config_aba_vc10_32bit_debug.mk
+++ makefiles/nmake/config_aba_vc10_32bit_debug.mk
@@ -21,26 +21,26 @@
 # version of the boost library
 
 #BOOST_VERSION = 1_46_1
-BOOST_VERSION = 1_46_0
+BOOST_VERSION = 1_49_0
 
 # base dir where boost is installed
 
 #BOOST_DIR = C:\Programme\boost\boost_$(BOOST_VERSION)
 #BOOST_DIR = C:\Program Files\boost\boost_$(BOOST_VERSION)
 #BOOST_DIR = D:\Programme\boost\boost_$(BOOST_VERSION)
-BOOST_DIR = C:\Cygwin\home\Andreas\boost_1_46_0-win64
+BOOST_DIR = C:\Cygwin\home\Andreas Baumann\boost_1_49_0-win32-debug
 #BOOST_DIR = D:\boost\boost_1_45_0
 #BOOST_DIR = D:\Program Files\boost\boost_$(BOOST_VERSION)
 
 # depends on the choosen boost layout
-BOOST_INCLUDE_DIR = $(BOOST_DIR)\Include\boost-1_46
+BOOST_INCLUDE_DIR = $(BOOST_DIR)\Include\boost-1_49
 #BOOST_INCLUDE_DIR = $(BOOST_DIR)
 
 # visual studio version used for compiling
 
 #BOOST_VC_VER = vc80
-BOOST_VC_VER = vc90
-#BOOST_VC_VER = vc100
+#BOOST_VC_VER = vc90
+BOOST_VC_VER = vc100
 
 # TODO: probe those
 #BOOST_MT = -mt
@@ -81,6 +81,9 @@ PGSQL_DIR = C:\Program Files\PostgreSQL\9.0
 #PGSQL_DIR = D:\Program Files\PostgreSQL\9.0
 #PGSQL_DIR = C:\cygwin\home\Andreas\postgresql-9.0.4-win64-debug
 
+# enable depending on libintl.dll and libiconv.dll (deployment only)
+PGDLL_WITHOUT_MAJOR_VERSION = 1
+
 # Tcl 8.5 and Expect
 ####################
 
@@ -96,6 +99,13 @@ EXPECT = $(TCL_DIR)\bin\tclsh.exe
 WIX_DIR = C:\Program Files (x86)\Windows Installer XML v3.5
 # @home PIII
 #WIX_DIR = C:\Program Files\Windows Installer XML v3.5
+
+#WIX_LIBS = dutil_2008.lib wcautil_2008.lib
+WIX_LIBS = dutil_2008_x64.lib wcautil_2008_x64.lib
+
+# architecture of resulting msi
+#WIX_ARCH = x86
+WIX_ARCH = x64
 
 # Doxygen binary to generate API documentation (for docu and deployment)
 ########################################################################
