0270e1259a7f04f785506bd7373a4f19cf649df7
Andreas Baumann <abaumann@yahoo.com>
2012-05-16 11:51:36 +0200

    -

--- tests/rlib/rlibfilter.cpp
+++ tests/rlib/rlibfilter.cpp
@@ -1,6 +1,14 @@
 #include "rlibfilter.hpp"
 
 #include <cstring>
+#include <iostream>
+
+using namespace std;
+
+static gint wolf_rlib_input_close( gpointer input_ptr )
+{
+	return 0;
+}
 
 static gint wolf_rlib_free_input_filter( gpointer input_ptr )
 {
@@ -9,8 +17,54 @@ static gint wolf_rlib_free_input_filter( gpointer input_ptr )
 	return 0;
 }
 
+static gint wolf_rlib_first( gpointer input_ptr, gpointer result_ptr )
+{
+	return FALSE;
+}
 
-void *wolf_rlib_new_result_from_query( gpointer input_ptr, gchar *query ) 
+static gint wolf_rlib_next( gpointer input_ptr, gpointer result_ptr )
+{
+	return FALSE;
+}
+
+static gint wolf_rlib_previous( gpointer input_ptr, gpointer result_ptr )
+{
+	return FALSE;
+}
+
+static gint wolf_rlib_last( gpointer input_ptr, gpointer result_ptr )
+{
+	return FALSE;
+}
+
+static gint wolf_rlib_isdone( gpointer input_ptr, gpointer result_ptr )
+{
+	return TRUE;
+}
+
+static const gchar* wolf_rlib_get_error( gpointer input_ptr )
+{
+	return "ERROR";
+}
+
+static void *wolf_rlib_new_result_from_query( gpointer input_ptr, gchar *query ) 
+{
+	cerr << "query: " << query << endl;
+	
+	return NULL;
+}
+
+static gchar *wolf_rlib_get_field_value_as_string( gpointer input_ptr, gpointer result_ptr, gpointer field_ptr )
+{
+	return "";
+}
+
+static gpointer wolf_rlib_resolve_field_pointer( gpointer input_ptr, gpointer result_ptr, gchar *name ) 
+{
+	return NULL; 
+}
+
+static void wolf_rlib_free_result( gpointer input_ptr, gpointer result_ptr )
 {
 }
 
@@ -21,23 +75,18 @@ gpointer wolf_rlib_new_input_filter( )
 	input = (struct input_filter *)g_malloc( sizeof( struct input_filter ) );
 	memset( input, 0, sizeof( struct input_filter ) );
 	input->_private = 0;
-	/*
-	input->input_close = rlib_csv_input_close;
-	input->first = rlib_csv_first;
-	input->next = rlib_csv_next;
-	input->previous = rlib_csv_previous;
-	input->last = rlib_csv_last;
-	input->isdone = rlib_csv_isdone;
-	input->get_error = rlib_csv_get_error;
-	* */
+	input->input_close = wolf_rlib_input_close;
+	input->first = wolf_rlib_first;
+	input->next = wolf_rlib_next;
+	input->previous = wolf_rlib_previous;
+	input->last = wolf_rlib_last;
+	input->isdone = wolf_rlib_isdone;
+	input->get_error = wolf_rlib_get_error;
 	input->new_result_from_query = wolf_rlib_new_result_from_query;
-	/*
-	input->get_field_value_as_string = rlib_csv_get_field_value_as_string;
-	input->resolve_field_pointer = rlib_csv_resolve_field_pointer;
-	*/
+	input->get_field_value_as_string = wolf_rlib_get_field_value_as_string;
+	input->resolve_field_pointer = wolf_rlib_resolve_field_pointer;
 	input->free = wolf_rlib_free_input_filter;
-/*
- * 	input->free_result = rlib_csv_rlib_free_result;*
-	*/
+	input->free_result = wolf_rlib_free_result;
+	
 	return input;
 }
