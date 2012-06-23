68ac924ba3aa525b07a3918394451af07df510e3
Mihai Barbos <mihai.barbos@gmail.com>
2012-06-23 21:36:22 +0200

    -

--- src/libwolframe/b64decode.c
+++ src/libwolframe/b64decode.c
@@ -7,14 +7,15 @@ For details, see http://sourceforge.net/projects/libb64
 
 #include <base64.h>
 
-static inline int base64_decode_value( char value_in )
+static const char decodeTable[] = { 62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51 };
+static const unsigned short decodeTableSize = sizeof( decodeTable );
+
+static inline int base64_decodeValue( char value )
 {
-	static const char decoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
-	static const char decoding_size = sizeof(decoding);
-	value_in -= 43;
-	if (value_in < 0 || value_in > decoding_size)
+	value -= 43;
+	if ( value < 0 || value > decodeTableSize )
 		return -1;
-	return decoding[(int)value_in];
+	return decodeTable[ (int)value ];
 }
 
 void base64_initDecodeState( base64_DecodeState* state )
@@ -31,54 +32,48 @@ int base64_decodeBlock( base64_DecodeState* state, const char* code_in, const in
 
 	*plainchar = state->plainchar;
 
-	switch (state->step)
-	{
-		while (1)
-		{
+	switch ( state->step )	{
+		while ( 1 )	{
 	case STEP_A:
 			do {
-				if (codechar == code_in+length_in)
-				{
+				if (codechar == code_in+length_in)	{
 					state->step = STEP_A;
 					state->plainchar = *plainchar;
 					return plainchar - plaintext_out;
 				}
-				fragment = (char)base64_decode_value(*codechar++);
+				fragment = (char)base64_decodeValue(*codechar++);
 			} while (fragment < 0);
 			*plainchar    = (fragment & 0x03f) << 2;
 	case STEP_B:
 			do {
-				if (codechar == code_in+length_in)
-				{
+				if (codechar == code_in+length_in)	{
 					state->step = STEP_B;
 					state->plainchar = *plainchar;
 					return plainchar - plaintext_out;
 				}
-				fragment = (char)base64_decode_value(*codechar++);
-			} while (fragment < 0);
+				fragment = (char)base64_decodeValue(*codechar++);
+			} while ( fragment < 0 );
 			*plainchar++ |= (fragment & 0x030) >> 4;
 			*plainchar    = (fragment & 0x00f) << 4;
 	case STEP_C:
 			do {
-				if (codechar == code_in+length_in)
-				{
+				if (codechar == code_in+length_in)	{
 					state->step = STEP_C;
 					state->plainchar = *plainchar;
 					return plainchar - plaintext_out;
 				}
-				fragment = (char)base64_decode_value(*codechar++);
+				fragment = (char)base64_decodeValue(*codechar++);
 			} while (fragment < 0);
 			*plainchar++ |= (fragment & 0x03c) >> 2;
 			*plainchar    = (fragment & 0x003) << 6;
 	case STEP_D:
 			do {
-				if (codechar == code_in+length_in)
-				{
+				if (codechar == code_in+length_in)	{
 					state->step = STEP_D;
 					state->plainchar = *plainchar;
 					return plainchar - plaintext_out;
 				}
-				fragment = (char)base64_decode_value(*codechar++);
+				fragment = (char)base64_decodeValue(*codechar++);
 			} while (fragment < 0);
 			*plainchar++   |= (fragment & 0x03f);
 		}
@@ -86,4 +81,3 @@ int base64_decodeBlock( base64_DecodeState* state, const char* code_in, const in
 	/* control should not reach here */
 	return plainchar - plaintext_out;
 }
-
--- src/libwolframe/b64encode.c
+++ src/libwolframe/b64encode.c
@@ -7,6 +7,8 @@ For details, see http://sourceforge.net/projects/libb64
 
 #include <base64.h>
 
+static const char* encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 void base64_initEncodeState(base64_EncodeState* state, unsigned short lineLength )
 {
 	state->step = STEP_0;
@@ -24,7 +26,6 @@ void base64_resetEncodeState(base64_EncodeState* state )
 
 static inline char base64_encodeValue( unsigned char value )
 {
-	static const char* encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 	if ( value > 63 )
 		return '=';
 	return encoding[ value ];
@@ -38,52 +39,47 @@ int base64_encodeBlock( base64_EncodeState* state, const unsigned char* plaintex
 	char result;
 	char fragment;
 
-	result = state ->result;
+	result = state->result;
 
-	switch (state ->step)
-	{
-		while (1)
-		{
-	case STEP_0:
-			if (plainchar == plaintextend)
-			{
-				state ->result = result;
-				state ->step = STEP_0;
-				return codechar - code_out;
-			}
-			fragment = *plainchar++;
-			result = (fragment & 0x0fc) >> 2;
-			*codechar++ = base64_encodeValue(result);
-			result = (fragment & 0x003) << 4;
-	case STEP_1:
-			if (plainchar == plaintextend)
-			{
-				state ->result = result;
-				state ->step = STEP_1;
-				return codechar - code_out;
-			}
-			fragment = *plainchar++;
-			result |= (fragment & 0x0f0) >> 4;
-			*codechar++ = base64_encodeValue(result);
-			result = (fragment & 0x00f) << 2;
-	case STEP_2:
-			if (plainchar == plaintextend)
-			{
-				state ->result = result;
-				state ->step = STEP_2;
-				return codechar - code_out;
-			}
-			fragment = *plainchar++;
-			result |= (fragment & 0x0c0) >> 6;
-			*codechar++ = base64_encodeValue(result);
-			result  = (fragment & 0x03f) >> 0;
-			*codechar++ = base64_encodeValue(result);
+	switch ( state->step )	{
+		while ( 1 )	{
+			case STEP_0:
+				if (plainchar == plaintextend)	{
+					state ->result = result;
+					state ->step = STEP_0;
+					return codechar - code_out;
+				}
+				fragment = *plainchar++;
+				result = (fragment & 0x0fc) >> 2;
+				*codechar++ = base64_encodeValue(result);
+				result = (fragment & 0x003) << 4;
+			case STEP_1:
+				if (plainchar == plaintextend)	{
+					state ->result = result;
+					state ->step = STEP_1;
+					return codechar - code_out;
+				}
+				fragment = *plainchar++;
+				result |= (fragment & 0x0f0) >> 4;
+				*codechar++ = base64_encodeValue(result);
+				result = (fragment & 0x00f) << 2;
+			case STEP_2:
+				if (plainchar == plaintextend)	{
+					state ->result = result;
+					state ->step = STEP_2;
+					return codechar - code_out;
+				}
+				fragment = *plainchar++;
+				result |= (fragment & 0x0c0) >> 6;
+				*codechar++ = base64_encodeValue(result);
+				result  = (fragment & 0x03f) >> 0;
+				*codechar++ = base64_encodeValue(result);
 
-			++( state->stepCount );
-			if ( state->stepCount == state->lineLength / 4 )	{
-				*codechar++ = '\n';
-				state->stepCount = 0;
-			}
+				++( state->stepCount );
+				if ( state->stepCount == state->lineLength / 4 )	{
+					*codechar++ = '\n';
+					state->stepCount = 0;
+				}
 		}
 	}
 	/* control should not reach here */
