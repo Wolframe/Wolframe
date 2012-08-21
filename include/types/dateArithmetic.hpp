36b103a3e55215e7aaf9cd2bff94f74eefd2ebeb
PatrickFrey <patrickpfrey@yahoo.com>
2012-08-21 13:40:27 +0200

    Added type for date arithmetics

---
+++ include/types/dateArithmetic.hpp
@@ -0,0 +1,88 @@
+/************************************************************************
+
+ Copyright (C) 2011, 2012 Project Wolframe.
+ All rights reserved.
+
+ This file is part of Project Wolframe.
+
+ Commercial Usage
+    Licensees holding valid Project Wolframe Commercial licenses may
+    use this file in accordance with the Project Wolframe
+    Commercial License Agreement provided with the Software or,
+    alternatively, in accordance with the terms contained
+    in a written agreement between the licensee and Project Wolframe.
+
+ GNU General Public License Usage
+    Alternatively, you can redistribute this file and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wolframe is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Wolframe.  If not, see <http://www.gnu.org/licenses/>.
+
+ If you have questions regarding the use of this file, please contact
+ Project Wolframe.
+
+************************************************************************/
+///\file types/dateArithmetic.hpp
+///\brief Interface to date arithmetic functions
+///\remark based on http://alcor.concordia.ca/~gpkatch/gdate-algorithm.html: Date arithmetics algorithm's described and implemented by Gary Katch (Implementation http://alcor.concordia.ca/~gpkatch/gdate-c.html)
+#ifndef _TYPES_DATE_ARITHMETIC_HPP_INCLUDED
+#define _TYPES_DATE_ARITHMETIC_HPP_INCLUDED
+#include <string>
+
+namespace _Wolframe {
+namespace types {
+
+///\class Date
+///\brief Type for date arithmetic
+class Date
+{
+public:
+	///\brief Constructor
+	///\param[in] y number of years. Smallest year allowed in 1600
+	///\param[in] m number of month counted from 1 (January)
+	///\param[in] d number of days counted from 1 (first day of month)
+	Date( unsigned short y, unsigned short m, unsigned short d);
+
+	///\brief Copy constructor
+	Date( const Date& o)
+		:m_daynum(o.m_daynum){}
+
+	///\brief Get the difference in days
+	int operator - (const Date& o)			{return (int)(m_daynum - o.m_daynum);}
+	///\brief Add number of days
+	Date operator + (int days)			{Date rt( m_daynum + days); rt.check(); return rt;}
+	///\brief Subtract number of days
+	Date operator - (int days)			{Date rt( m_daynum - days); rt.check(); return rt;}
+	///\brief Add number of days
+	Date& operator += (int days)			{m_daynum += days; check(); return *this;}
+	///\brief Subtract number of days
+	Date& operator -= (int days)			{m_daynum -= days; check(); return *this;}
+
+	///\brief Get the date as a formated string
+	///\param[in] format strftime - format string
+	///\return the date as string
+	std::string tostring( const char* format) const;
+
+private:
+	///\brief Check the date and throw an exception if the date is not in the boundaries allowed by this library (bigger than 1599/12/31)
+	void check();
+	///\brief Constructor
+	Date( long daynum)
+		:m_daynum(daynum){}
+
+	///\brief Representation as number of days
+	long m_daynum;
+};
+
+
+}}
+#endif
+
---
+++ src/types_dateArithmetic.cpp
@@ -0,0 +1,134 @@
+/************************************************************************
+
+ Copyright (C) 2011, 2012 Project Wolframe.
+ All rights reserved.
+
+ This file is part of Project Wolframe.
+
+ Commercial Usage
+    Licensees holding valid Project Wolframe Commercial licenses may
+    use this file in accordance with the Project Wolframe
+    Commercial License Agreement provided with the Software or,
+    alternatively, in accordance with the terms contained
+    in a written agreement between the licensee and Project Wolframe.
+
+ GNU General Public License Usage
+    Alternatively, you can redistribute this file and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wolframe is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Wolframe.  If not, see <http://www.gnu.org/licenses/>.
+
+ If you have questions regarding the use of this file, please contact
+ Project Wolframe.
+
+************************************************************************/
+///\file types_dateArithmetic.cpp
+///\brief Date arithmetic functions
+///\remark based on http://alcor.concordia.ca/~gpkatch/gdate-algorithm.html: Date arithmetics algorithm's described and implemented by Gary Katch
+#include "types/dateArithmetic.hpp"
+#include <stdexcept>
+#include <cstring>
+
+using namespace _Wolframe::types;
+
+namespace
+{
+struct sdate
+{
+	long y;
+	long m;
+	long d;
+};
+}
+
+///\brief Convert date to day number
+///\remark Original source from http://alcor.concordia.ca/~gpkatch/gdate-c.html
+static long daynum( sdate d)
+{
+	long y, m;
+
+	m = (d.m + 9)%12;	/* mar=0, feb=11 */
+	y = d.y - m/10;		/* if Jan/Feb, year-- */
+	return y*365 + y/4 - y/100 + y/400 + (m*306 + 5)/10 + (d.d - 1);
+}
+
+static sdate g_mindate = {1600, 1, 1};
+static sdate g_sundaydate = {1600, 1, 2};
+static long g_mindaynum = daynum( g_mindate);
+static long g_sundaydaynum = daynum( g_sundaydate);
+
+static short weekday( long d)
+{
+	return (d - g_sundaydaynum + 7) % 7;
+}
+
+///\brief Convert day number to y,m,d format
+///\remark Original source from http://alcor.concordia.ca/~gpkatch/gdate-c.html
+struct sdate dtf( long d)
+{
+	struct sdate pd;
+	long y, ddd, mi;
+
+	y = (10000*d + 14780)/3652425;
+	ddd = d - (y*365 + y/4 - y/100 + y/400);
+	if (ddd < 0)
+	{
+		y--;
+		ddd = d - (y*365 + y/4 - y/100 + y/400);
+	}
+	mi = (52 + 100*ddd)/3060;
+	pd.y = y + (mi + 2)/12;
+	pd.m = (mi + 2)%12 + 1;
+	pd.d = ddd - (mi*306 + 5)/10 + 1;
+	return pd;
+}
+
+
+void Date::check()
+{
+	if (m_daynum < g_mindaynum)
+	{
+		throw std::runtime_error( "bad date in calculation");
+	}
+}
+
+Date::Date( unsigned short y, unsigned short m, unsigned short d)
+{
+	sdate dt;
+	dt.y = y;
+	dt.m = m;
+	dt.d = d;
+	m_daynum = daynum( dt);
+	check();
+}
+
+std::string Date::tostring( const char* format) const
+{
+	char buf[ 1024];
+	if (strlen( format) > 50) throw std::bad_alloc();
+	struct tm tm_info;
+	std::memset( &tm_info, 0, sizeof(tm_info));
+	sdate dt = dtf( m_daynum);
+	sdate dt_first_jan;
+	dt_first_jan.y = dt.y;
+	dt_first_jan.m = 1;
+	dt_first_jan.d = 1;
+	tm_info.tm_mday = dt.d;
+	tm_info.tm_mon = dt.m;
+	tm_info.tm_year = dt.y;
+	tm_info.tm_wday = weekday( m_daynum);
+	tm_info.tm_yday = m_daynum - daynum( dt_first_jan);
+
+	strftime( buf, sizeof(buf), format, &tm_info);
+	return std::string( buf);
+}
+
+
