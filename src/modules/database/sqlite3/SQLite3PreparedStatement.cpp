746bcf42971999fd02a469b58209f7db93dd1cc3
Andreas Baumann <abaumann@yahoo.com>
2012-08-25 16:24:47 +0200

    reverted V2 patch in sqlite3

--- src/modules/database/sqlite3/SQLite3PreparedStatement.cpp
+++ src/modules/database/sqlite3/SQLite3PreparedStatement.cpp
@@ -39,6 +39,9 @@
 #include <cstdlib>
 #include <limits>
 #include <stdexcept>
+#if SQLITE_VERSION_NUMBER < 3005000
+#error This SQLite version is not supported by this module. It relies on the "
+#endif
 
 using namespace _Wolframe;
 using namespace _Wolframe::db;
@@ -86,11 +89,7 @@ bool PreparedStatementHandler_sqlite3::executeInstruction( const char* stmstr, S
 {
 	sqlite3_stmt* inst = 0;
 	const char *stmtail;
-#if SQLITE_VERSION_NUMBER >= 3005000
 	int rc = sqlite3_prepare_v2( m_conn, stmstr, -1, &inst, &stmtail);
-#else
-	int rc = sqlite3_prepare( m_conn, stmstrm -1, &inst, &stmtail);
-#endif
 
 	if (rc != SQLITE_OK) return status( rc, newstate);
 
