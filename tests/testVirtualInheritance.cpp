64331374ee37c611812177b37d1258cc7ffc25e0
Andreas Baumann <abaumann@yahoo.com>
2012-10-02 10:39:49 +0200

    added a virtual inheritance test

---
+++ tests/testVirtualInheritance.cpp
@@ -0,0 +1,59 @@
+//
+// Test if the compilers support virtual inheritance on two inheritance pathes
+//
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+using namespace std;
+
+class Animal
+{
+	public:
+		Animal( ) { cout << "Constructing animal.." << endl; }
+		virtual ~Animal( ) { cout << "Destroyin animal.." << endl; }
+
+		virtual void fly( ) = 0;
+		virtual void trot( ) = 0;
+		
+};
+
+class Horse : public virtual Animal
+{
+	public:
+		Horse( ) { cout << "Constructing horse.." << endl; }
+		virtual ~Horse( ) { cout << "Destroying horse.." << endl; }
+		virtual void trot( ) { cout << "I'm trotting" << endl; }
+};
+
+class Bird : public virtual Animal
+{
+	public:
+		Bird( ) { cout << "Constructing bird.." << endl; }
+		virtual ~Bird( ) { cout << "Destroying bird.." << endl; }
+		virtual void fly( ) { cout << "I'm flying" << endl; }
+};
+
+class Pegasus : public virtual Horse, public virtual Bird
+{
+	public:
+		Pegasus( ) { cout << "Constructing pegasus.." << endl; }
+		virtual ~Pegasus( ) { cout << "Destroying pegasus.." << endl; }
+};
+
+TEST( VirtualInheritance, test )
+{
+	Animal *p = new Pegasus( );
+	p->trot( );
+	p->fly( );
+	delete p;
+}
+
+
+int main( int argc, char **argv )
+{
+	::testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS();
+}
+
