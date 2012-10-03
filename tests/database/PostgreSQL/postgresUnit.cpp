a286c74fcc16b6c6f812458973bfc6d355555958
Mihai Barbos <mihai.barbos@gmail.com>
2012-10-03 17:42:18 +0200

    splited database tests into directories

---
+++ tests/database/PostgreSQL/postgresUnit.cpp
@@ -0,0 +1,54 @@
+//
+// testing the Module Loader
+//
+
+#include "logger-v1.hpp"
+#include "moduleDirectory.hpp"
+#include "gtest/gtest.h"
+
+#include "PostgreSQL.hpp"
+
+using namespace _Wolframe::db;
+using namespace _Wolframe::log;
+
+_Wolframe::log::LogBackend*	logBackendPtr;
+
+// The fixture for testing Wolframe module that log
+class PQmoduleFixture : public ::testing::Test
+{
+	LogBackend& logBack;
+
+	protected:
+		PQmoduleFixture( ) :
+			logBack( LogBackend::instance( ) )
+		{
+			logBack.setConsoleLevel( LogLevel::LOGLEVEL_DATA );
+			logBackendPtr = &logBack;
+		}
+};
+
+TEST_F( PQmoduleFixture, CreatePostgreSQLunit_0 )
+{
+	PostgreSQLdbUnit db( "testDB", "localhost", 0, "wolframe",
+			     "wolfusr", "wolfpwd", "", "", "", "", "",
+			     3, 4, 3, 10, "program" );
+	ASSERT_STREQ( "PostgreSQL", db.className());
+	ASSERT_STREQ( "testDB", db.ID().c_str());
+	ASSERT_FALSE( db.loadProgram());
+}
+
+TEST_F( PQmoduleFixture, CreatePostgreSQLunit_1 )
+{
+	PostgreSQLdbUnit db( "testDB", "blabla", 0, "wolframe",
+			     "wolfusr", "wolfpwd", "", "", "", "", "",
+			     3, 4, 3, 10, "" );
+	ASSERT_STREQ( "PostgreSQL", db.className());
+	ASSERT_STREQ( "testDB", db.ID().c_str());
+	ASSERT_TRUE( db.loadProgram());
+}
+
+int main( int argc, char **argv )
+{
+	::testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS( );
+}
