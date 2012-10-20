7097459105689ebc8b51cae2d18a91489b56d687
Mihai Barbos <mihai.barbos@gmail.com>
2012-10-21 01:36:34 +0200

    added ubuntu 12.10 to the supported platforms

--- makefiles/gmake/platform.mk
+++ makefiles/gmake/platform.mk
@@ -271,6 +271,10 @@ endif
 # Ubuntu
 ifeq "$(LINUX_DIST)" "ubuntu"
 
+ifeq "$(LINUX_REV)" "12.10"
+XSLT_MAN_STYLESHEET ?= /usr/share/xml/docbook/stylesheet/nwalsh/manpages/docbook.xsl
+endif
+
 ifeq "$(LINUX_REV)" "12.04"
 XSLT_MAN_STYLESHEET ?= /usr/share/xml/docbook/stylesheet/nwalsh/manpages/docbook.xsl
 endif
@@ -359,6 +363,13 @@ endif
 
 # Ubuntu
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+BOOST_DIR ?= /usr
+BOOST_INCLUDE_DIR ?= $(BOOST_DIR)/include
+BOOST_LIB_DIR ?= $(BOOST_DIR)/lib
+BOOST_LIBRARY_TAG ?=
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 BOOST_DIR ?= /usr
@@ -606,6 +617,10 @@ endif
 
 ifeq "$(LINUX_DIST)" "ubuntu"
 
+ifeq "$(LINUX_REV)" "12.10"
+OPENSSL_LIBS ?= -lssl -lcrypto
+endif
+
 ifeq "$(LINUX_REV)" "12.04"
 OPENSSL_LIBS ?= -lssl -lcrypto
 endif
@@ -822,6 +837,17 @@ endif
 # Ubuntu
 
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+QT_DIR ?= /usr
+QT_INCLUDE_DIR ?= $(QT_DIR)/include/qt4
+QT_LIB_DIR ?= $(QT_DIR)/lib
+QT_BIN_DIR ?= $(QT_DIR)/bin
+QT_MOC ?= $(QT_BIN_DIR)/moc
+QT_LRELEASE ?= $(QT_BIN_DIR)/lrelease
+QT_LUPDATE ?= $(QT_BIN_DIR)/lupdate
+QT_LDFLAGS =
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 QT_DIR ?= /usr
@@ -1053,6 +1079,13 @@ endif
 
 ifeq "$(LINUX_DIST)" "ubuntu"
 
+ifeq "$(LINUX_REV)" "12.10"
+PAM_DIR ?= /usr
+PAM_INCLUDE_DIR ?= $(PAM_DIR)/include
+PAM_LIB_DIR ?= /lib
+PAM_LIBS ?= -lpam
+endif
+
 ifeq "$(LINUX_REV)" "12.04"
 PAM_DIR ?= /usr
 PAM_INCLUDE_DIR ?= $(PAM_DIR)/include
@@ -1218,6 +1251,13 @@ endif
 # Ubuntu
 
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+SASL_DIR ?= /usr
+SASL_INCLUDE_DIR ?= $(SASL_DIR)/include
+SASL_LIB_DIR ?= $(SASL_DIR)/lib
+SASL_LIBS ?= -lsasl2
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 SASL_DIR ?= /usr
@@ -1395,6 +1435,13 @@ endif
 # Ubuntu
 
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+SQLITE3_DIR ?= /usr
+SQLITE3_INCLUDE_DIR ?= $(SQLITE3_DIR)/include
+SQLITE3_LIB_DIR ?= $(SQLITE3_DIR)/lib
+SQLITE3_LIBS ?= -lsqlite3
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 SQLITE3_DIR ?= /usr
@@ -1586,6 +1633,15 @@ endif
 
 # Ubuntu
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+PGSQL_DIR ?= /usr
+PGSQL_INCLUDE_DIR ?= $(PGSQL_DIR)/include/postgresql
+PGSQL_INCLUDE_DIRS = -I$(PGSQL_INCLUDE_DIR)
+PGSQL_LIB_DIR ?= $(PGSQL_DIR)/lib
+PGSQL_LIB_DIRS = -L$(PGSQL_LIB_DIR)
+PGSQL_LIBS ?= -lpq
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 PGSQL_DIR ?= /usr
@@ -1783,6 +1839,15 @@ endif
 # Ubuntu
 
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+LIBXML2_DIR ?= /usr
+LIBXML2_INCLUDE_DIR ?= $(LIBXML2_DIR)/include/libxml2
+LIBXML2_INCLUDE_DIRS = -I$(LIBXML2_INCLUDE_DIR)
+LIBXML2_LIB_DIR ?= $(LIBXML2_DIR)/lib
+LIBXML2_LIB_DIRS = -L$(LIBXML2_LIB_DIR)
+LIBXML2_LIBS ?= -lxml2
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 LIBXML2_DIR ?= /usr
@@ -1985,6 +2050,15 @@ endif
 # Ubuntu
 
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+LIBXSLT_DIR ?= /usr
+LIBXSLT_INCLUDE_DIR ?= $(LIBXSLT_DIR)/include
+LIBXSLT_INCLUDE_DIRS = -I$(LIBXSLT_INCLUDE_DIR)
+LIBXSLT_LIB_DIR ?= $(LIBXSLT_DIR)/lib
+LIBXSLT_LIB_DIRS = -L$(LIBXSLT_LIB_DIR)
+LIBXSLT_LIBS ?= -lxslt
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 LIBXSLT_DIR ?= /usr
@@ -2191,6 +2265,15 @@ endif
 
 ifeq "$(LINUX_DIST)" "ubuntu"
 
+ifeq "$(LINUX_REV)" "12.10"
+LIBHPDF_DIR ?= NOT SUPPLIED ON THIS PLATFORM
+LIBHPDF_INCLUDE_DIR ?= NOT SUPPLIED ON THIS PLATFORM
+LIBHPDF_INCLUDE_DIRS = NOT SUPPLIED ON THIS PLATFORM
+LIBHPDF_LIB_DIR ?= NOT SUPPLIED ON THIS PLATFORM
+LIBHPDF_LIB_DIRS = NOT SUPPLIED ON THIS PLATFORM
+LIBHPDF_LIBS ?= NOT SUPPLIED ON THIS PLATFORM
+endif
+
 ifeq "$(LINUX_REV)" "12.04"
 LIBHPDF_DIR ?= NOT SUPPLIED ON THIS PLATFORM
 LIBHPDF_INCLUDE_DIR ?= NOT SUPPLIED ON THIS PLATFORM
@@ -2386,6 +2469,15 @@ endif
 # Ubuntu
 
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+LIBPNG_DIR ?= /usr
+LIBPNG_INCLUDE_DIR ?= $(LIBPNG_DIR)/include
+LIBPNG_INCLUDE_DIRS = -I$(LIBPNG_INCLUDE_DIR)
+LIBPNG_LIB_DIR ?= $(LIBPNG_DIR)/lib
+LIBPNG_LIB_DIRS = -L$(LIBPNG_LIB_DIR)
+LIBPNG_LIBS ?= -lpng
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 LIBPNG_DIR ?= /usr
@@ -2595,6 +2687,15 @@ endif
 
 ifeq "$(LINUX_DIST)" "ubuntu"
 
+ifeq "$(LINUX_REV)" "12.10"
+LIBZ_DIR ?= /usr
+LIBZ_INCLUDE_DIR ?= $(LIBZ_DIR)/include
+LIBZ_INCLUDE_DIRS = -I$(LIBZ_INCLUDE_DIR)
+LIBZ_LIB_DIR ?= $(LIBZ_DIR)/lib
+LIBZ_LIB_DIRS = -L$(LIBZ_LIB_DIR)
+LIBZ_LIBS ?= -lz
+endif
+
 ifeq "$(LINUX_REV)" "12.04"
 LIBZ_DIR ?= /usr
 LIBZ_INCLUDE_DIR ?= $(LIBZ_DIR)/include
@@ -2802,6 +2903,15 @@ endif
 # Ubuntu
 
 ifeq "$(LINUX_DIST)" "ubuntu"
+
+ifeq "$(LINUX_REV)" "12.10"
+ICU_DIR ?= /usr
+ICU_INCLUDE_DIR ?= $(ICU_DIR)/include
+ICU_INCLUDE_DIRS = -I$(ICU_INCLUDE_DIR)
+ICU_LIB_DIR ?= $(ICU_DIR)/lib
+ICU_LIB_DIRS = -L$(ICU_LIB_DIR)
+ICU_LIBS ?=
+endif
 
 ifeq "$(LINUX_REV)" "12.04"
 ICU_DIR ?= /usr
