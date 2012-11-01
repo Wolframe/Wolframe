ea97b83b9f38fa27648c5f449644c5285e8dbb9c
Andreas Baumann <abaumann@yahoo.com>
2012-11-01 11:20:44 +0100

    added a simple boost-locale test

--- tests/boost-locale/boostLocaleTest.cpp
+++ tests/boost-locale/boostLocaleTest.cpp
@@ -1,107 +1,36 @@
 //
-// testing the Module Loader
+// testing the boost-locale module with ICU
 //
 
-#include "logger-v1.hpp"
-#include "moduleDirectory.hpp"
 #include "gtest/gtest.h"
 
-#include "tests/mod_test/common.hpp"
-#include "tests/mod_test_containers/common.hpp"
+#include <boost/locale/generator.hpp>
+#include <boost/locale.hpp>
 
 #include <string>
-#include <list>
-
-using namespace _Wolframe::module;
-using namespace _Wolframe::log;
-using namespace _Wolframe;
-using namespace std;
 
 // The fixture for testing class _Wolframe::module
-class ModuleFixture : public ::testing::Test
+class BoostLocaleFixture : public ::testing::Test
 {
 	protected:
-		LogBackend& logBack;
+		boost::locale::generator m_gen;
 
 	protected:
-		ModuleFixture( ) :
-			logBack( LogBackend::instance( ) )
+		BoostLocaleFixture( )
 		{
-			logBack.setConsoleLevel( LogLevel::LOGLEVEL_DATA );
+			m_gen.locale_cache_enabled( true );
+			boost::locale::generator gen;
+			std::locale loc = gen( "" );
+			std::locale::global( loc );
 		}
 };
 
-TEST_F( ModuleFixture, LoadingModuleFromDir )
+TEST_F( BoostLocaleFixture, SimpleTest )
 {
-	ModulesDirectory modDir;
-	list<string> modFiles;
-
-#ifndef _WIN32
-	modFiles.push_back( "./tests/mod_test/mod_test.so" );
-#else
-	modFiles.push_back( ".\\tests\\mod_test\\mod_test.dll" );
-#endif
-	bool res = LoadModules( modDir, modFiles );
-	ASSERT_TRUE( res );
-
-	ConfiguredBuilder* builder = modDir.getBuilder( "TestObject" );
-	ASSERT_TRUE( builder != NULL );
-
-	config::NamedConfiguration* configuration = builder->configuration( "TestObject" );
-	ASSERT_TRUE( configuration != NULL );
-
-	ConfiguredObjectConstructor< test::TestUnit >* cnstrctr = dynamic_cast< ConfiguredObjectConstructor< test::TestUnit >* >( builder->constructor( ) );
-	ASSERT_TRUE( cnstrctr != NULL );
-	test::TestUnit* unit = cnstrctr->object( *configuration );
-
-	string s = unit->hello( );
-	ASSERT_EQ( s, "hello" );
-
-	delete configuration;
-	delete unit;
-}
-
-TEST_F( ModuleFixture, LoadingModuleWithMultipleContainers )
-{
-	ModulesDirectory modDir;
-	list<string> modFiles;
-
-#ifndef _WIN32
-	modFiles.push_back( "./tests/mod_test_containers/mod_test_containers.so" );
-#else
-	modFiles.push_back( ".\\tests\\mod_test_containers\\mod_test_containers.dll" );
-#endif
-	bool res = LoadModules( modDir, modFiles );
-	ASSERT_TRUE( res );
-
-	ConfiguredBuilder* builder1 = modDir.getBuilder( "TestObject1" );
-	ASSERT_TRUE( builder1 != NULL );
-
-	ConfiguredBuilder* builder2 = modDir.getBuilder( "TestObject2" );
-	ASSERT_TRUE( builder2 != NULL );
-
-	config::NamedConfiguration* configuration1 = builder1->configuration( "TestObject1" );
-	ASSERT_TRUE( configuration1 != NULL );
-
-	config::NamedConfiguration* configuration2 = builder2->configuration( "TestObject2" );
-	ASSERT_TRUE( configuration2 != NULL );
-
-	ConfiguredObjectConstructor< test_containers::TestUnit1 >* cnstrctr1 = dynamic_cast< ConfiguredObjectConstructor< test_containers::TestUnit1 >* >( builder1->constructor( ) );
-	ASSERT_TRUE( cnstrctr1 != NULL );
-	test_containers::TestUnit1* unit1 = cnstrctr1->object( *configuration1 );
-
-	ConfiguredObjectConstructor< test_containers::TestUnit2 >* cnstrctr2 = dynamic_cast< ConfiguredObjectConstructor< test_containers::TestUnit2 >* >( builder2->constructor( ) );
-	ASSERT_TRUE( cnstrctr2 != NULL );
-	test_containers::TestUnit2* unit2 = cnstrctr2->object( *configuration2 );
-
-	string s1 = unit1->hello( );
-	ASSERT_EQ( s1, "hello" );
-
-	string s2 = unit2->hullo( );
-	ASSERT_EQ( s2, "hullo" );
-
-	delete unit1;
-	delete unit2;
+	const std::string input = "grüßen";
+	std::string output = boost::locale::to_upper( input );
+	const std::string must = "GRÜSSEN";
+	EXPECT_EQ( output, must );
 }
 
 int main( int argc, char **argv )
