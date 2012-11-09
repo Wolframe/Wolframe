e7984d5cc256f3d621996189478700a5ff6aa9ff
Mihai Barbos <mihai.barbos@gmail.com>
2012-11-09 17:30:52 +0100

    changed the SQLite test to use the form database structure

--- examples/configurator/postgres.sql
+++ examples/configurator/postgres.sql
@@ -1,55 +1,139 @@
---
+
 -- The categories tree
 --
-
 CREATE TABLE Category	(
 	ID		SERIAL	PRIMARY KEY,
-	parent		INT	REFERENCES tree( ID ),
-	name		TEXT,
+	parent		INT	REFERENCES Category( ID ),
+	name		TEXT	NOT NULL,
+	normalizedName	TEXT	NOT NULL UNIQUE,
 	lft		INT	NOT NULL UNIQUE DEFERRABLE CHECK ( lft > 0 ),
 	rgt		INT	NOT NULL UNIQUE DEFERRABLE CHECK ( rgt > 1 ),
 	CONSTRAINT order_check CHECK ( rgt > lft )
 );
 
--- Insert a category as a subcategory of PARENT
-CREATE FUNCTION addCategory( parentID int, name text )
-RETURNS void
-AS $$
-DECLARE parentRgt	INTEGER;		-- rgt of the parent
-BEGIN
-parentRgt := ( SELECT rgt FROM Category WHERE ID = parentID );
+-- The features tree
+--
+CREATE TABLE Feature	(
+	ID		SERIAL	PRIMARY KEY,
+	parent		INT	REFERENCES Feature( ID ),
+	name		TEXT	NOT NULL,
+	normalizedName	TEXT	NOT NULL UNIQUE,
+	lft		INT	NOT NULL UNIQUE DEFERRABLE CHECK ( lft > 0 ),
+	rgt		INT	NOT NULL UNIQUE DEFERRABLE CHECK ( rgt > 1 ),
+	CONSTRAINT order_check CHECK ( rgt > lft )
+);
 
-UPDATE Category SET rgt = CASE
-			WHEN rgt >= parentRgt THEN rgt + 2
-			ELSE rgt
-		END,
-		lft = CASE
-			WHEN lft > parentRgt THEN lft + 2
-			ELSE lft
-		END
-	WHERE rgt >= parentRgt;
+-- The list of manufacturers
+--
+CREATE TABLE Manufacturer	(
+	ID		SERIAL	PRIMARY KEY,
+	name		TEXT	NOT NULL,
+	normalizedName	TEXT	NOT NULL UNIQUE
+);
 
-INSERT INTO Category ( parent, name, lft, rgt )
-	VALUES ( parentID, name, parentRgt, ( parentRgt + 1 ));
-END;
-$$ LANGUAGE plpgsql;
+-- The list of components
+--
+CREATE TABLE Components	(
+	ID		SERIAL	PRIMARY KEY,
+	code		TEXT	NOT NULL UNIQUE,
+	name		TEXT	NOT NULL,
+	normalizedName	TEXT	NOT NULL UNIQUE,
+	category	INT	REFERENCES Category( ID ),
+	manufacturerID	INT	REFERENCES Manufacturer( ID ),
+	mfgCode		TEXT
+);
 
--- Delete a category and all its subcategories
-CREATE FUNCTION deleteCategory( categoryID int )
-RETURNS void
-AS $$
-DECLARE
-	leftMargin	INTEGER;		-- the left margin of the subtree
-	rightMargin	INTEGER;		-- the right margin of the subtree
-	width		INTEGER;		-- the total width of the subtree
-BEGIN
-SELECT lft, rgt INTO leftMargin, rightMargin FROM Category WHERE ID = categoryID;
-width := rightMargin - leftMargin + 1;
+-- The list of features required by members of a category
+--
+CREATE TABLE CategRequires	(
+	Category	INT	REFERENCES Category( ID ),
+	Feature		INT	REFERENCES Feature( ID ),
+	MinQuantity	INT,
+	MaxQuantity	INT
+);
 
-DELETE FROM Category WHERE lft BETWEEN leftMargin AND rightMargin;
+-- The list of features provided by members of a category
+--
+CREATE TABLE CategProvides	(
+	Category	INT	REFERENCES Category( ID ),
+	Feature		INT	REFERENCES Feature( ID ),
+	MinQuantity	INT,
+	MaxQuantity	INT
+);
 
-UPDATE Category SET lft = lft - width WHERE lft > rightMargin;
-UPDATE Category SET rgt = rgt - width WHERE rgt > rightMargin;
-END;
-$$ LANGUAGE plpgsql;
+-- The list of checks for members of a category
+--
+CREATE TABLE CategoryCheck	(
+	Category	INT	REFERENCES Category( ID ),
+	RuleName	TEXT	NOT NULL,
+	normalizedName	TEXT	NOT NULL UNIQUE
+);
+
+-- The list of features required by a component
+--
+CREATE TABLE ComponentRequires	(
+	Category	INT	REFERENCES Category( ID ),
+	Feature		INT	REFERENCES Feature( ID ),
+	MinQuantity	INT,
+	MaxQuantity	INT
+);
+
+-- The list of features provided by a component
+--
+CREATE TABLE ComponentProvides	(
+	Category	INT	REFERENCES Category( ID ),
+	Feature		INT	REFERENCES Feature( ID ),
+	MinQuantity	INT,
+	MaxQuantity	INT
+);
+
+-- The list of checks for a component
+--
+CREATE TABLE ComponentCheck	(
+	Category	INT	REFERENCES Category( ID ),
+	RuleName	TEXT
+);
+
+-- Receipes
+--
+CREATE TABLE Receipe	(
+	ID		SERIAL	PRIMARY KEY,
+	name		TEXT	NOT NULL,
+	normalizedName	TEXT	NOT NULL UNIQUE,
+	description	TEXT,
+	categoryID	INT	REFERENCES Category( ID ),
+	CreationDate	TIMESTAMP,
+	Comment		TEXT
+);
+
+CREATE TABLE ReceipeContent	(
+	receipeID	INT	REFERENCES Receipe( ID ),
+	categoryID	INT	REFERENCES Category( ID ),
+	MinQuantity	INT,
+	MaxQuantity	INT,
+	Comment		TEXT
+);
+
+
+-- Configuration
+--
+CREATE TABLE Configuration	(
+	ID		SERIAL	PRIMARY KEY,
+	categoryID	INT	REFERENCES Category( ID ),
+	name		TEXT,
+	description	TEXT,
+	comment		TEXT
+);
+
+CREATE TABLE ConfigComponent	(
+	configID	INT	REFERENCES Configuration( ID ),
+	componentID	INT	REFERENCES Components( ID ),
+	quantity	INT
+);
+
+CREATE TABLE ComposedConfig	(
+	configID	INT	REFERENCES Configuration( ID ),
+	subConfigID	INT	REFERENCES Configuration( ID ),
+	quantity	INT
+);
 
--- tests/database/SQLite3/testDB.sql
+++ tests/database/SQLite3/testDB.sql
@@ -1,4 +1,61 @@
-CREATE TABLE test (
-	ID	int primary key,
-	name	text
+-- The platforms tree
+--
+CREATE TABLE Platform	(
+	ID		SERIAL	PRIMARY KEY,
+	parent		INT	REFERENCES Platform( ID ),
+	name		TEXT,
+	lft		INT,
+	rgt		INT,
+	CONSTRAINT order_check CHECK ( rgt > lft )
+);
+
+INSERT INTO Platform ( ID, parent, name, lft, rgt ) VALUES ( 1, NULL, 'Android', 1, 2 );
+INSERT INTO Platform ( ID, parent, name, lft, rgt ) VALUES ( 2, NULL, 'iOS', 3, 4 );
+INSERT INTO Platform ( ID, parent, name, lft, rgt ) VALUES ( 3, NULL, 'Windows Mobile', 5, 6 );
+INSERT INTO Platform ( ID, parent, name, lft, rgt ) VALUES ( 4, NULL, 'Desktop', 7, 14 );
+INSERT INTO Platform ( ID, parent, name, lft, rgt ) VALUES ( 5, 4, 'Linux', 8, 9 );
+INSERT INTO Platform ( ID, parent, name, lft, rgt ) VALUES ( 6, 4, 'Windows', 10, 11 );
+INSERT INTO Platform ( ID, parent, name, lft, rgt ) VALUES ( 7, 4, 'Mac OS', 12, 13 );
+
+-- Table of locales
+--
+CREATE TABLE locale (
+	ID		TEXT	PRIMARY KEY,
+	name		TEXT,
+	localName	TEXT
+);
+
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'en', 'English', 'English' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'en-GB', 'English (United Kingdom)', 'English (United Kingdom)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'en-US', 'English (United States)', 'English (United States)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'fi', 'Finnish', 'suomi' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'fi-FI', 'Finnish (Finland)', 'suomi (Suomi)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'fr', 'French', 'français' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'fr-FR', 'French (France)', 'français (France)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'fr-CH', 'French (Switzerland)', 'français (Suisse)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'de', 'German', 'Deutsch' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'de-DE', 'German (Germany)', 'Deutsch (Deutschland)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'de-LI', 'German (Liechtenstein)', 'Deutsch (Liechtenstein)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'de-CH', 'German (Switzerland)', 'Deutsch (Schweiz)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'it', 'Italian', 'italiano' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'it-IT', 'Italian (Italy)', 'italiano (Italia)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'it-CH', 'Italian (Switzerland)', 'italiano (Svizzera)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'ro', 'Romanian', 'română' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'ro-RO', 'Romanian (Romania)', 'română (România)' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'rm', 'Romansh', 'Rumantsch' );
+INSERT INTO locale ( ID, name, localName ) VALUES ( 'rm-CH', 'Romansh (Switzerland)', 'Rumantsch (Svizra)' );
+
+CREATE TABLE formName (
+	ID		SERIAL	PRIMARY KEY,
+	name		TEXT	NOT NULL,
+	normalizedName	TEXT	NOT NULL UNIQUE
+);
+
+
+CREATE TABLE form (
+	ID		INT	REFERENCES formName( ID ),
+	Platform	INT	REFERENCES Platform( ID ),
+	locale		TEXT	REFERENCES locale( ID ),
+	bersion		INT,
+	body		TEXT
 );
