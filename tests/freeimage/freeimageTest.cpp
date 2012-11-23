ba9bfde0e551a292492760dc07f2f085a6daabd0
Andreas Baumann <abaumann@yahoo.com>
2012-11-23 10:16:24 +0100

    improved the free image test, added version checks and width, height

--- tests/freeimage/freeimageTest.cpp
+++ tests/freeimage/freeimageTest.cpp
@@ -5,13 +5,14 @@
 #include "gtest/gtest.h"
 
 #include <iostream>
+#include <sstream>
 
 #include "FreeImage.h"
 
-static void FreeImageErrorHandler( FREE_IMAGE_FORMAT fif, const char *message ) {
+static void FreeImageErrorHandler( FREE_IMAGE_FORMAT format, const char *message ) {
 	std::cerr << "\n*** ";
-	if(fif != FIF_UNKNOWN) {
-		std::cerr << FreeImage_GetFormatFromFIF(fif) << " Format\n";
+	if(format != FIF_UNKNOWN) {
+		std::cerr << FreeImage_GetFormatFromFIF(format) << " Format\n";
 	}
 	std::cerr << message;
 	std::cerr << " ***\n";
@@ -41,9 +42,49 @@ class FreeImageFixture : public ::testing::Test
 		}
 };
 
-TEST_F( FreeImageFixture, SimpleTest )
+TEST_F( FreeImageFixture, VersionInfo )
 {
-//	EXPECT_EQ( output, must );
+	//std::cout << "FreeImage " << FreeImage_GetVersion( ) << "\n";
+	//std::cout << FreeImage_GetCopyrightMessage( ) << "\n\n";	
+
+	const char *version = FreeImage_GetVersion( );
+	std::ostringstream ss;
+	ss << FREEIMAGE_MAJOR_VERSION << "." << FREEIMAGE_MINOR_VERSION << "." << FREEIMAGE_RELEASE_SERIAL;
+	ASSERT_STREQ( version, ss.str( ).c_str( ) );
+}
+
+#define TESTFILE "test.png"
+
+TEST_F( FreeImageFixture, ImageInfo )
+{
+// load the image
+	FREE_IMAGE_FORMAT format = FIF_UNKNOWN;
+
+// check the file signature and deduce its format
+// (the second argument is currently not used by FreeImage)
+	format = FreeImage_GetFileType( TESTFILE, 0 );
+	if( format == FIF_UNKNOWN ) {
+// no signature ?
+// try to guess the file format from the file extension
+		format = FreeImage_GetFIFFromFilename( TESTFILE );
+	}
+	
+	FIBITMAP *image;
+	
+// check that the plugin has reading capabilities ...
+	if( ( format != FIF_UNKNOWN ) && FreeImage_FIFSupportsReading( format ) ) {
+// ok, let's load the file
+		image = FreeImage_Load( format, TESTFILE, 0 );
+	}
+	
+	unsigned int width = FreeImage_GetWidth( image );
+	unsigned int height = FreeImage_GetHeight( image );
+	//std::cout << "size is " << width << " x " << height << std::cout;
+	ASSERT_EQ( width, 312 );
+	ASSERT_EQ( height, 312 );
+	
+// Unload the bitmap
+	FreeImage_Unload( image );
 }
 
 int main( int argc, char **argv )
