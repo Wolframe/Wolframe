854a325e4c9f6d0349099b56286815d3d333e5b2
Andreas Baumann <abaumann@yahoo.com>
2012-11-24 10:19:47 +0100

    fixes in clean in makefiles on Windows added better messages to see where nmake looses time

--- makefiles/nmake/clean.mk
+++ makefiles/nmake/clean.mk
@@ -11,27 +11,27 @@
 # - target: distclean
 
 clean_recursive:
-	@if not "$(SUBDIRS)" == "" @for %%d IN ( $(SUBDIRS) ) do @cd %%d & $(MAKE) /nologo /f Makefile.w32 clean & cd ..
+	@if not "$(SUBDIRS)" == "" @for %%d IN ( $(SUBDIRS) ) do @echo Entering directory '%%CD%\%%d' doing 'clean' & cd %%d & $(MAKE) /nologo /f Makefile.w32 clean & cd ..
 
 clean: clean_recursive local_clean
 	-@erase *.bak 2>NUL
 	-@erase *~ 2>NUL
 	-@erase *.exe 2>NUL
 	-@erase *.exe.manifest 2>NUL
-	-@del /s *.obj >NUL 2>NUL
+	-@erase *.obj >NUL 2>NUL
 	-@erase *.pdb 2>NUL
 	-@erase *.res 2>NUL
 	-@erase MSG*.bin 2>NUL
 	-@erase *.dllobj 2>NUL
 	-@erase *.dll 2>NUL
 	-@erase *.lib 2>NUL
-	-@del /s *.dllobj >NUL 2>NUL
+	-@erase *.dllobj >NUL 2>NUL
 	-@erase *.exp 2>NUL
 	-@erase *.ilk 2>NUL
 	-@erase *.idb 2>NUL
 	-@erase *.manifest 2>NUL
 
 distclean_recursive:
-	@if not "$(SUBDIRS)" == "" @for %%d IN ( $(SUBDIRS) ) do @cd %%d & $(MAKE) /nologo /f Makefile.w32 distclean & cd ..
+	@if not "$(SUBDIRS)" == "" @for %%d IN ( $(SUBDIRS) ) do @echo Entering directory '%%CD%\%%d' doing 'distclean' & cd %%d & $(MAKE) /nologo /f Makefile.w32 distclean & cd ..
 
 distclean: distclean_recursive local_distclean clean
