404d08ef15121612c70f7abb16e8d950456e4707
Andreas Baumann <abaumann@yahoo.com>
2012-11-28 11:02:56 +0100

    made tags add and edit work for pictures

--- examples/demo/configurator/program/postgres.sql
+++ examples/demo/configurator/program/postgres.sql
@@ -1,3 +1,6 @@
 -- Select the last ID created in the Picture table
 -- PF:HACK: Because we have no variables, we have also to reserve a result for what we whould store in a 'variable'
 PREPARE getLastPictureID AS SELECT $1,currval( pg_get_serial_sequence( 'picture', 'id'));
+
+-- for pictures without cats or feats
+PREPARE getLastInsertedPictureID AS SELECT DISTINCT currval( pg_get_serial_sequence( 'picture', 'id' ) );
