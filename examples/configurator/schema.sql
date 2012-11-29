7f918bee75b8bcae16e321f7591243d6a0048f25
Andreas Baumann <abaumann@yahoo.com>
2012-11-29 12:53:57 +0100

    fixed picture postgresql "problem"

--- examples/configurator/schema.sql
+++ examples/configurator/schema.sql
@@ -23,8 +23,8 @@ CREATE TABLE Picture	(
 	info		TEXT,
 	width		INT,
 	height		INT,
-	image		BYTEA,
-	thumbnail	BYTEA
+	image		TEXT,
+	thumbnail	TEXT
 );
 
 CREATE TABLE PictureTag	(
@@ -225,3 +225,17 @@ CREATE TABLE ComposedConfig	(
 	UNIQUE ( configID, subConfigID )
 );
 
+CREATE OR REPLACE FUNCTION _group_concat(text, text)
+RETURNS text AS $$
+	SELECT CASE
+		WHEN $2 IS NULL THEN $1
+		WHEN $1 IS NULL THEN $2
+	ELSE $1 operator(pg_catalog.||) ',' operator(pg_catalog.||) $2
+END
+$$ IMMUTABLE LANGUAGE SQL;
+
+CREATE AGGREGATE group_concat (
+	BASETYPE = text,
+	SFUNC = _group_concat,
+	STYPE = text
+);
