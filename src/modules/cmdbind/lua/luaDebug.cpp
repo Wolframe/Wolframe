d2a090613dfbab8aeff984d57963a9396b322b75
PatrickFrey <patrickpfrey@yahoo.com>
2012-12-06 07:22:11 +0100

    removed unused code in lua debug

--- src/modules/cmdbind/lua/luaDebug.cpp
+++ src/modules/cmdbind/lua/luaDebug.cpp
@@ -45,43 +45,6 @@ extern "C" {
 #include "lua.h"
 }
 
-static void iterate_and_print(lua_State *L, int index, std::string indent)
-{
-    // Push another reference to the table on top of the stack (so we know
-    // where it is, and this function can work for negative, positive and
-    // pseudo indices
-    lua_pushvalue(L, index);
-    // stack now contains: -1 => table
-    lua_pushnil(L);
-    // stack now contains: -1 => nil; -2 => table
-    while (lua_next(L, -2))
-    {
-        // stack now contains: -1 => value; -2 => key; -3 => table
-        // copy the key so that lua_tostring does not modify the original
-        lua_pushvalue(L, -2);
-        // stack now contains: -1 => key; -2 => value; -3 => key; -4 => table
-        const char *key = lua_tostring(L, -1);
-        if (lua_istable( L, -2))
-        {
-		std::cout << indent << (key?key:"*") << ":" << std::endl;
-		iterate_and_print( L, -2, indent + "   ");
-	}
-	else
-	{
-		const char *value = lua_tostring(L, -2);
-		std::cout << indent << (key?key:"*") << " " << (value?value:"*") << std::endl;
-	}
-        // pop value + copy of key, leaving original key
-        lua_pop(L, 2);
-        // stack now contains: -1 => key; -2 => table
-    }
-    // stack now contains: -1 => table (when lua_next returns 0 it pops the key
-    // but does not push anything.)
-    // Pop table
-    lua_pop(L, 1);
-    // Stack is now the same as it was on entry to this function
-}
-
 static void getDescription_( lua_State *ls, int index, std::string& rt)
 {
 	int type = lua_type( ls, index);
@@ -136,8 +99,6 @@ static void getDescription_( lua_State *ls, int index, std::string& rt)
 			{
 				throw std::runtime_error( "lua stack overflow");
 			}
-			iterate_and_print( ls, index, " ");
-
 			rt.append( "{ ");
 			lua_pushvalue( ls, index);
 			lua_pushnil( ls);
