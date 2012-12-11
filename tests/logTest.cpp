02f8b6c3a5cd36ef8579b2491468e58593cdc62b
Mihai Barbos <mihai.barbos@gmail.com>
2012-12-11 04:32:50 +0100

    added log level tests

--- tests/logTest.cpp
+++ tests/logTest.cpp
@@ -18,8 +18,19 @@ using namespace _Wolframe::log;
 // The fixture for testing class _Wolframe::log
 class LoggingFixture : public ::testing::Test
 {
-protected:
-	LogBackend& logBack;
+public:
+	void setLevel( const LogLevel::Level level )	{
+		// in order not to spoil the output of gtest :-)
+		logBack.setConsoleLevel( LogLevel::LOGLEVEL_UNDEFINED );
+
+		logBack.setLogfileLevel( level );
+		logBack.setSyslogLevel( level );
+#if defined( _WIN32 )
+		logBack.setWinDebugLevel( level );
+		logBack.setEventlogLevel( level );
+#endif // defined( _WIN32 )
+
+	}
 
 protected:
 	LoggingFixture( ) :
@@ -43,6 +54,9 @@ protected:
 		logBack.setEventlogLog( "Application" );
 #endif // defined( _WIN32 )
 	}
+
+protected:
+	LogBackend& logBack;
 };
 
 
@@ -58,7 +72,7 @@ public:
 // testB throws runtime_error when the << operator is called
 class testB	{
 public:
-	testB()	{ std::cerr << "testB constructor called"; }
+//	testB()	{ std::cerr << "testB constructor called"; }
 
 	friend std::ostream& operator << ( std::ostream& o, const testB& )
 		{  throw std::runtime_error( "testB << operator called" ); }
@@ -129,6 +143,7 @@ TEST_F( LoggingFixture, LogSystemErrorMarkersWin )
 
 TEST_F( LoggingFixture, LogLevelData )
 {
+	setLevel( LogLevel::LOGLEVEL_DATA );
 	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
 	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
 	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
@@ -140,7 +155,328 @@ TEST_F( LoggingFixture, LogLevelData )
 	EXPECT_THROW( LOG_DEBUG <<  testA(), std::logic_error );
 	EXPECT_THROW( LOG_TRACE <<  testA(), std::logic_error );
 	EXPECT_THROW( LOG_DATA <<  testA(), std::logic_error );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ERROR <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_WARNING <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_NOTICE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_INFO <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_DEBUG <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_TRACE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_DATA <<  testB(), std::runtime_error );
 }
+
+TEST_F( LoggingFixture, LogLevelTrace )
+{
+	setLevel( LogLevel::LOGLEVEL_TRACE );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_SEVERE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_ERROR <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_WARNING <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_NOTICE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_INFO <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_DEBUG <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_TRACE <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ERROR <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_WARNING <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_NOTICE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_INFO <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_DEBUG <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_TRACE <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelDebug )
+{
+	setLevel( LogLevel::LOGLEVEL_DEBUG );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_SEVERE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_ERROR <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_WARNING <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_NOTICE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_INFO <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_DEBUG <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ERROR <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_WARNING <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_NOTICE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_INFO <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_DEBUG <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelInfo )
+{
+	setLevel( LogLevel::LOGLEVEL_INFO );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_SEVERE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_ERROR <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_WARNING <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_NOTICE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_INFO <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ERROR <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_WARNING <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_NOTICE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_INFO <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelNotice )
+{
+	setLevel( LogLevel::LOGLEVEL_NOTICE );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_SEVERE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_ERROR <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_WARNING <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_NOTICE <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ERROR <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_WARNING <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_NOTICE <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelWarning )
+{
+	setLevel( LogLevel::LOGLEVEL_WARNING );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_SEVERE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_ERROR <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_WARNING <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testA() );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ERROR <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_WARNING <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testB() );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelError )
+{
+	setLevel( LogLevel::LOGLEVEL_ERROR );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_SEVERE <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_ERROR <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_WARNING <<  testA() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testA() );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ERROR <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_WARNING <<  testB() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testB() );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelSevere )
+{
+	setLevel( LogLevel::LOGLEVEL_SEVERE );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_SEVERE <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_ERROR <<  testA() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testA() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testA() );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_SEVERE <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_ERROR <<  testB() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testB() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testB() );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelCritical )
+{
+	setLevel( LogLevel::LOGLEVEL_CRITICAL );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testA() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testA() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testA() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testA() );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_THROW( LOG_CRITICAL <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testB() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testB() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testB() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testB() );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelAlert )
+{
+	setLevel( LogLevel::LOGLEVEL_ALERT );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_THROW( LOG_ALERT <<  testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_CRITICAL <<  testA() );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testA() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testA() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testA() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testA() );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_THROW( LOG_ALERT <<  testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_CRITICAL <<  testB() );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testB() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testB() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testB() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testB() );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelFatal )
+{
+	setLevel( LogLevel::LOGLEVEL_FATAL );
+	EXPECT_THROW( LOG_FATAL << testA(), std::logic_error );
+	EXPECT_NO_THROW( LOG_ALERT <<  testA() );
+	EXPECT_NO_THROW( LOG_CRITICAL <<  testA() );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testA() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testA() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testA() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testA() );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_THROW( LOG_FATAL << testB(), std::runtime_error );
+	EXPECT_NO_THROW( LOG_ALERT <<  testB() );
+	EXPECT_NO_THROW( LOG_CRITICAL <<  testB() );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testB() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testB() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testB() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testB() );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
+TEST_F( LoggingFixture, LogLevelUndefined )
+{
+	setLevel( LogLevel::LOGLEVEL_UNDEFINED );
+	EXPECT_NO_THROW( LOG_FATAL << testA() );
+	EXPECT_NO_THROW( LOG_ALERT <<  testA() );
+	EXPECT_NO_THROW( LOG_CRITICAL <<  testA() );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testA() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testA() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testA() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testA() );
+	EXPECT_NO_THROW( LOG_INFO <<  testA() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testA() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testA() );
+	EXPECT_NO_THROW( LOG_DATA <<  testA() );
+
+	EXPECT_NO_THROW( LOG_FATAL << testB() );
+	EXPECT_NO_THROW( LOG_ALERT <<  testB() );
+	EXPECT_NO_THROW( LOG_CRITICAL <<  testB() );
+	EXPECT_NO_THROW( LOG_SEVERE <<  testB() );
+	EXPECT_NO_THROW( LOG_ERROR <<  testB() );
+	EXPECT_NO_THROW( LOG_WARNING <<  testB() );
+	EXPECT_NO_THROW( LOG_NOTICE <<  testB() );
+	EXPECT_NO_THROW( LOG_INFO <<  testB() );
+	EXPECT_NO_THROW( LOG_DEBUG <<  testB() );
+	EXPECT_NO_THROW( LOG_TRACE <<  testB() );
+	EXPECT_NO_THROW( LOG_DATA <<  testB() );
+}
+
 
 int main( int argc, char **argv )
 {
