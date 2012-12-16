124e9d43eb764adfb47a49d8701df735c6218110
Andreas Baumann <abaumann@yahoo.com>
2012-12-16 12:06:52 +0100

    -

--- makefiles/gmake/dochelp.mk
+++ makefiles/gmake/dochelp.mk
@@ -7,7 +7,7 @@ make doc-doxygen            build HTML and man version of doxygen documentation
 make doc-html               build HTML version of the manual (one big html only)
 make doc-html-chunked       build chunked HTML version of the manual
 make doc-pdf                build PDF version of the manual
-make epub                   build EPUB version of the manual
+make doc-epub               build EPUB version of the manual
 
 make check                  test the validity of the documenation source and tools
 
