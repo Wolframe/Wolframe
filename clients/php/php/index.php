87ebb8c59566fa3f1cbb25db684a4b0f587c5c4d
Andreas Baumann <abaumann@yahoo.com>
2013-02-12 16:09:07 +0100

    playing with php CRM tutorial, small fix: customer not custemers for widget name in listCustomers.ui added local configuration and makefile for CRM tutorial

--- clients/php/php/index.php
+++ clients/php/php/index.php
@@ -1,3 +1,17 @@
 <?php
-	echo $_GET["username"];
+	require_once "WolframeClient.php";
+
+	$connParams = array(
+		"host" => "localhost",
+		"port" => "7661"
+	);
+	$conn = new WolframeClient( $connParams );
+
+	if( !$conn->connect( ) ) {
+		print "Unable to connect to Wolframe server: " . $conn->getLastError( );
+		return;
+	}
 	
+	$conn->disconnect( );
+	
+	echo $_GET["username"];
