7501e1d2d99dce8b2f1d8876790b47c7a3e0d06d
Andreas Baumann <abaumann@yahoo.com>
2013-06-26 12:11:42 +0200

    started to split boost versions Debian/Ubuntu packaging (work in progress)

--- contrib/osc/deploy_to_osc.sh
+++ contrib/osc/deploy_to_osc.sh
@@ -3,22 +3,31 @@
 VERSION=0.0.1
 OSC_HOME=$HOME/home:andreas_baumann/Wolframe
 
+# the original package
 rm -f wolframe-$VERSION.tar.gz
 make \
 	WITH_SSL=1 WITH_EXPECT=1 WITH_QT=1 WITH_PAM=1 WITH_SASL=1 \
 	WITH_SQLITE3=1 WITH_PGSQL=1 WITH_LUA=1 WITH_LIBXML2=1 WITH_LIBXSLT=1 \
 	dist-gz
 cp wolframe-$VERSION.tar.gz $OSC_HOME/wolframe_$VERSION.tar.gz
+
+# the Redhat build script
 cp redhat/wolframe.spec $OSC_HOME/wolframe.spec
 
+# patches
 cp contrib/osc/boost/boost_1_48_0-gcc-compile.patch $OSC_HOME/.
 
+# compute sizes of packages
 SIZE=`stat -c '%s' $OSC_HOME/wolframe_$VERSION.tar.gz`
 CHKSUM=`md5sum $OSC_HOME/wolframe_$VERSION.tar.gz | cut -f 1 -d' '`
 
-cat contrib/osc/wolframe.dsc > $OSC_HOME/wolframe.dsc
-echo " $CHKSUM $SIZE wolframe_$VERSION.tar.gz" >> $OSC_HOME/wolframe.dsc
+# copy all Debian versions of the description files.
+cp contrib/osc/wolframe*.dsc $OSC_HOME
+for i in `ls $OSC_HOME/wolframe*.dsc`; do
+	echo " $CHKSUM $SIZE wolframe_$VERSION.tar.gz" >> $i
+done
 
+# Archlinux specific files
 cat contrib/osc/PKGBUILD > $OSC_HOME/PKGBUILD
 cp archlinux/wolframe.conf $OSC_HOME/wolframe.conf
 cp archlinux/wolframed.service $OSC_HOME/wolframed.service
