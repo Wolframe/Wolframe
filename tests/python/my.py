71e7808787012f423f0a710ad41a9f398e3317fd
Andreas Baumann <abaumann@yahoo.com>
2013-08-29 10:17:53 +0200

    fooling around with Python unicode

--- tests/python/my.py
+++ tests/python/my.py
@@ -4,5 +4,13 @@ def func( i, f ):
     print( 'func', i, f )
     return i * f
 
+# test UTF-8 identifiers (especially important for function names to work
+répertoire = "/tmp/records.log"
+print( 'repertoire: ', répertoire )
+
+# output Unicode string
+funny = 'Ѧηḓґℯα﹩ ℬ@üмαηη αт ℯʊґøṧ℘їḓ℮ґ▣ḉ◎м'
+print( funny.encode('ascii', 'backslashreplace') )
+
 time = 1377677003.4642606
 print( 'Module initialized on ' , ctime( time ) )
