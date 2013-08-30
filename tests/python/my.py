bd4f2e5b74661563c9509bfc5d58c121f86a1057
Andreas Baumann <abaumann@yahoo.com>
2013-08-30 11:19:56 +0200

    more python unicode tests

--- tests/python/my.py
+++ tests/python/my.py
@@ -1,8 +1,12 @@
 from time import time, ctime
+import sys
 
 def func( i, f ):
     print( 'func', i, f )
     return i * f
+
+# this is 'ANSI_X3.4-1968' on Linux
+print( sys.stdout.encoding )
 
 # test UTF-8 identifiers (especially important for function names to work
 répertoire = "/tmp/records.log"
@@ -10,7 +14,10 @@ print( 'repertoire: ', répertoire )
 
 # output Unicode string
 funny = 'α♭αʊмαᾔᾔ αт ⑂α♄øø ∂◎т ¢☺м'
-print( funny.encode('ascii', 'backslashreplace') )
+print( funny.encode( 'ascii', 'backslashreplace') )
+
+# this works only if PYTHONIOENCODING=utf-8 and LANG=en_US.utf8 is set
+#print( funny )
 
 time = 1377677003.4642606
 print( 'Module initialized on ' , ctime( time ) )
