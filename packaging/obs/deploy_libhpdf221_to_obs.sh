6d8c8fc12e7a5431e15ae4ca9f24a4286b9e873b
Andreas Baumann <abaumann@yahoo.com>
2013-10-19 22:13:06 +0200

    building xUbuntu 13.10 package against system libhpdf

---
+++ packaging/obs/deploy_libhpdf221_to_obs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+OSC_HOME=$HOME/home:andreas_baumann/libhpdf221
+
+if test "x$TMPDIR" = "x"; then
+	TMPDIR=/tmp
+fi
+
+cp packaging/obs/libhpdf221/libhpdf221-*.dsc $OSC_HOME
+for i in `ls $OSC_HOME/libhpdf221-*.dsc`; do
+	OS_ORIG=`echo $i | cut -f 2 -d '-' | sed 's/\.dsc$//'`
+	OS=`echo $i | cut -f 2 -d '-' | sed 's/\.dsc$//' | tr -d '_'`
+	rm -rf $OSC_HOME/libhpdf_2.2.1-$OS.debian.tar.gz
+	rm -rf $TMPDIR/debian
+	cp -a packaging/obs/libhpdf221/debian-$OS_ORIG $TMPDIR/.
+	OLDDIR=$PWD
+	cd $TMPDIR
+	mv debian-$OS_ORIG debian
+	tar zcf $TMPDIR/libhpdf_2.2.1-$OS.debian.tar.gz debian
+	cd $OLDDIR
+	rm -rf $TMPDIR/debian
+	mv $TMPDIR/libhpdf_2.2.1-$OS.debian.tar.gz $OSC_HOME
+	SIZE=`stat -c '%s' $OSC_HOME/libhpdf_2.2.1-$OS.debian.tar.gz`
+	CHKSUM=`md5sum $OSC_HOME/libhpdf_2.2.1-$OS.debian.tar.gz | cut -f 1 -d' '`
+	echo " $CHKSUM $SIZE libhpdf_2.2.1-$OS.debian.tar.gz" >> $i
+done
---
+++ packaging/obs/libhpdf221/libhpdf221-xUbuntu_13.10.dsc
@@ -0,0 +1,13 @@
+Format: 3.0 (quilt)
+Source: libhpdf
+Binary: libhpdf-dev, libhpdf-2.2.1
+Architecture: any
+Version: 2.2.1-xUbuntu13.10
+Maintainer: Johan Van de Wauw <johan.vandewauw@gmail.com>
+Homepage: http://www.libharu.org
+Standards-Version: 3.9.2
+Vcs-Browser: http://git.debian.org/?p=collab-maint/libharu.git
+Vcs-Git: git://git.debian.org/collab-maint/libharu.git
+Build-Depends: debhelper (>= 7.0.50~), libpng-dev, libz-dev
+Files: 
+ 63da8e087744f1d6cf16f9501b1cb766 967530 libhpdf_2.2.1.orig.tar.gz
