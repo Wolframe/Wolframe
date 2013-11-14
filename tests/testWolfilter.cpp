6c31d631cf3f2cd9b05ce1a9e781316da501b8ed
patrickfrey <patrickpfrey@yahoo.com>
2013-11-14 17:22:04 +0100

    more output if thrown exception expected does not match in wolfilter test

--- tests/testWolfilter.cpp
+++ tests/testWolfilter.cpp
@@ -201,6 +201,7 @@ TEST_F( WolfilterTest, tests)
 						ee.close();
 
 						boost::this_thread::sleep( boost::posix_time::seconds( 3));
+						std::cerr << "exception part '" << *pi << "' not found in '" << ap << "'" << std::endl;
 						EXPECT_EQ( pi, pe);
 					}
 				}
