05c4d463a23d3b38f78b9d1c9a1de1b2c10160ce
Patrick Frey <patrickpfrey@yahoo.com>
2014-02-06 16:03:37 +0100

    ..

--- tests/dotnet/csharp/Functions/data/GetIdPair.lua
+++ tests/dotnet/csharp/Functions/data/GetIdPair.lua
@@ -1,10 +1,9 @@
 
 function GetIdPair( inp_)
 	logger.printc( "START function GetIdPair")
-	rt = inp_:table().place
-	logger.printc( "INPUT ", rt)
-	rt.a = 123
-	rt.b = 987
+	inp = inp_:table().place
+	logger.printc( "INPUT ", inp)
+	rt = { a = 123, b = 987 }
 	return rt
 end
 
