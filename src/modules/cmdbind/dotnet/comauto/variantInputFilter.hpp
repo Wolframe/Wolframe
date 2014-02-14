15574adcd4899836b2e075828771a2f8a2e974a4
Mihai Barbos <mihai.barbos@gmail.com>
2014-02-14 12:06:40 +0100

    updated the copyright notice

--- src/modules/cmdbind/dotnet/comauto/variantInputFilter.hpp
+++ src/modules/cmdbind/dotnet/comauto/variantInputFilter.hpp
@@ -1,5 +1,5 @@
 /************************************************************************
- Copyright (C) 2011 - 2013 Project Wolframe.
+ Copyright (C) 2011 - 2014 Project Wolframe.
  All rights reserved.
 
  This file is part of Project Wolframe.
