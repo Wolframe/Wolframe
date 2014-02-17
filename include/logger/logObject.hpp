79da63602f08ac952cd33b5fb5525313e81188fb
Mihai Barbos <mihai.barbos@gmail.com>
2014-02-17 12:29:09 +0100

    ...

--- include/logger/logObject.hpp
+++ include/logger/logObject.hpp
@@ -31,8 +31,10 @@ Project Wolframe.
 ************************************************************************/
 ///\file logger/logObject.hpp
 ///\brief Provides uniform handling of logger and exception objects
-#ifndef _Wolframe_LOGGER_OBJECT_HPP_INCLUDED
-#define _Wolframe_LOGGER_OBJECT_HPP_INCLUDED
+
+#ifndef _Wolframe_LOG_OBJECT_HPP_INCLUDED
+#define _Wolframe_LOG_OBJECT_HPP_INCLUDED
+
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -128,6 +130,6 @@ public:
 };
 
 
-}} //namespace
-#endif
+}} //namespace _Wolframe::log
 
+#endif	// _Wolframe_LOG_OBJECT_HPP_INCLUDED
