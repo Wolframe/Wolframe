216a4998abc30cdfa4a74d2553d66fb9f6d3d19c
patrickfrey <patrickpfrey@yahoo.com>
2014-03-08 13:48:50 +0100

    fixed typos

--- src/modules/authentication/textfile/TextFileAuth.hpp
+++ src/modules/authentication/textfile/TextFileAuth.hpp
@@ -66,7 +66,7 @@ public:
 	virtual const char* className() const		{ return TEXT_FILE_AUTHENTICATION_CLASS_NAME; }
 
 	/// methods
-	bool parse( const config::ConfigurationTree& pt, const std::string& node,
+	bool parse( const config::ConfigurationNode& pt, const std::string& node,
 		    const module::ModulesDirectory* modules );
 	bool check() const;
 	void print( std::ostream& os, size_t indent ) const;
