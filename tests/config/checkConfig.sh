a81732f03048e063eeef18130d284fa9b0d78716
Andreas Baumann <abaumann@yahoo.com>
2014-03-12 11:48:24 +0100

    renamed expect script from *.exp to *.ex

--- tests/config/checkConfig.sh
+++ tests/config/checkConfig.sh
@@ -4,7 +4,7 @@ EXPECT=$1
 
 cat configTests.lst | ( while read conf ret msg; do
 #        echo "shell script: expect script called with: $ret, $msg, $conf"
-        $EXPECT ./checkConfig.exp $conf $ret "$msg"
+        $EXPECT ./checkConfig.ex $conf $ret "$msg"
         if [ "$?" != "0" ]; then
                 exit 1
         fi
