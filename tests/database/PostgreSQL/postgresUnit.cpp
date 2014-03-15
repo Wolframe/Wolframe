d8649b4c4fbe76476820fad5c27d8bea162ae8fb
patrickfrey <patrickpfrey@yahoo.com>
2014-03-15 17:37:08 +0100

    fix signed/unsigned comparison warnings

--- tests/database/PostgreSQL/postgresUnit.cpp
+++ tests/database/PostgreSQL/postgresUnit.cpp
@@ -167,7 +167,7 @@ TEST_F( PQmoduleFixture, ExecuteInstruction )
 	trans->begin( );
 	Transaction::Result emptyres = trans->executeStatement( "SELECT * FROM TestTest");
 	trans->commit( );
-	EXPECT_EQ( emptyres.size(), 0);
+	EXPECT_EQ( emptyres.size(), 0U);
 
 	// ok transaction with statements with commit
 	trans->begin( );
@@ -178,8 +178,8 @@ TEST_F( PQmoduleFixture, ExecuteInstruction )
 	trans->begin( );
 	Transaction::Result res = trans->executeStatement( "SELECT * FROM TestTest ORDER BY id IS NULL, id ASC");
 	trans->commit( );
-	EXPECT_EQ( res.size(), 5);
-	EXPECT_EQ( res.colnames().size(), 4);
+	EXPECT_EQ( res.size(), 5U);
+	EXPECT_EQ( res.colnames().size(), 4U);
 	EXPECT_STREQ( "id", res.colnames().at(0).c_str());
 	EXPECT_STREQ( "name", res.colnames().at(1).c_str());
 	EXPECT_STREQ( "active", res.colnames().at(2).c_str());
@@ -220,7 +220,7 @@ TEST_F( PQmoduleFixture, ExecuteInstruction )
 				ASSERT_EQ( ri->at(1).type(), types::Variant::String);
 				ASSERT_EQ( ri->at(2).type(), types::Variant::Bool);
 				ASSERT_EQ( ri->at(3).type(), types::Variant::Double);
-				EXPECT_EQ( _WOLFRAME_MAX_INTEGER, ri->at(0).touint());
+				EXPECT_EQ( (_WOLFRAME_UINTEGER)_WOLFRAME_MAX_INTEGER, ri->at(0).touint());
 				double price( ri->at(3).todouble());
 				ASSERT_DOUBLE_EQ( std::numeric_limits<double>::max( ) / 2, price );
 				break;
@@ -406,8 +406,8 @@ TEST_F( PQmoduleFixture, ReusedBindParameter )
 
 	trans->begin( );
 	Transaction::Result res = trans->executeStatement( "SELECT * FROM TestTest");
-	EXPECT_EQ( res.size(), 1);
-	EXPECT_EQ( res.colnames().size(), 3);
+	EXPECT_EQ( res.size(), 1U);
+	EXPECT_EQ( res.colnames().size(), 3U);
 	EXPECT_STREQ( "id", res.colnames().at(0).c_str());
 	EXPECT_STREQ( "id2", res.colnames().at(1).c_str());
 	EXPECT_STREQ( "id3", res.colnames().at(2).c_str());
@@ -445,8 +445,8 @@ TEST_F( PQmoduleFixture, ExpressionWithParametersAndTypeCoercion )
 
 	trans->begin( );
 	Transaction::Result res = trans->executeStatement( "SELECT * FROM TestTest");
-	EXPECT_EQ( res.size(), 1);
-	EXPECT_EQ( res.colnames().size(), 3);
+	EXPECT_EQ( res.size(), 1U);
+	EXPECT_EQ( res.colnames().size(), 3U);
 	EXPECT_STREQ( "id", res.colnames().at(0).c_str());
 	EXPECT_STREQ( "id2", res.colnames().at(1).c_str());
 	EXPECT_STREQ( "id3", res.colnames().at(2).c_str());
