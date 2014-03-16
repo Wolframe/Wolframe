c93beafe5afc5a6c92a4492e3e3952bd7cdb4b99
Andreas Baumann <abaumann@yahoo.com>
2014-03-16 10:27:59 +0100

    reverted an 'unsigned int' to 'unsigned' in Postgresql.hpp

--- src/modules/database/postgresql/PostgreSQL.hpp
+++ src/modules/database/postgresql/PostgreSQL.hpp
@@ -185,7 +185,7 @@ private:
 	size_t			m_noConnections;	//< number of connections
 	PostgreSQLserverSettings m_serverSettings;	//< data like protocol settings, OIDs, etc. loaded at initialization from server
 	ObjectPool< PGconn* >	m_connPool;		//< pool of connections
-	unsigned int		m_statementTimeout;	//< default statement execution timeout
+	unsigned		m_statementTimeout;	//< default statement execution timeout
 	PostgreSQLdatabase	m_db;			//< real database object
 	PostgreSQLprogram	m_program;		//< (not supported yet) Loader of programs (m_programFiles)
 	std::list<std::string>	m_programFiles;		//< (not supported yet) list of source files with SQL statements to load at startup
