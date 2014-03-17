e9eea709ecda417b5610ec865a0dac7a2a719639
Andreas Baumann <abaumann@yahoo.com>
2014-03-17 20:12:31 +0100

    commented out some code which clang says it is not required

--- include/serialize/struct/parseValue.hpp
+++ include/serialize/struct/parseValue.hpp
@@ -94,6 +94,7 @@ struct ParseValueType
 };
 
 
+/* clang says unused
 static bool getBool( bool& val, int boolnum)
 {
 	if (boolnum == 0)
@@ -108,6 +109,7 @@ static bool getBool( bool& val, int boolnum)
 	}
 	return false;
 }
+*/
 
 template <typename ValueType>
 static bool parseValue_( ValueType& val, const ParseValueType::String&, const types::VariantConst& element)
