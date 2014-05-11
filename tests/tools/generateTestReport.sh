7238c72fdfb918fe14b617cad7d4c1f730036bba
Andreas Baumann <abaumann@yahoo.com>
2014-05-11 11:08:34 +0200

    generateTestReport fixed for BSD shells

--- tests/tools/generateTestReport.sh
+++ tests/tools/generateTestReport.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo ""
 echo "Test report"
 echo "-----------"
 echo ""
@@ -8,8 +7,12 @@ echo "Summary"
 echo "-------"
 echo ""
 
+echo "0" > _nof_total_tests
+echo "0" > _nof_total_errors
 find . -name gtestReport.txt -exec cat {} \; | sort | \
 while read test status nof_tests nof_errors; do
+	nof_total_tests=`cat _nof_total_tests`
+	nof_total_errors=`cat _nof_total_errors`
 	nof_total_tests=`expr $nof_total_tests + $nof_tests`
 	echo $nof_total_tests > _nof_total_tests
 	nof_total_errors=`expr $nof_total_errors + $nof_errors`
@@ -30,7 +33,6 @@ echo "Test results"
 echo "------------"
 echo ""
 find . -name gtestReport.txt -exec cat {} \; | sort
-echo ""
 
 echo "Test result details"
 echo "-------------------"
@@ -42,5 +44,4 @@ while read test status nof_tests nof_errors; do
 	find . -name "$test*.xml" -exec cat {} \;
 done
 echo "</testresults>"
-echo ""
 
