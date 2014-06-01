07130a68b37ca55322bb1ac3a4f7275a5782658a
Mihai Barbos <mihai.barbos@gmail.com>
2014-06-01 14:04:38 +0200

    ...

--- .Attic/php-client/test-userHash.php
+++ .Attic/php-client/test-userHash.php
@@ -33,42 +33,14 @@
 
 ************************************************************************/
 
-function userHash( $username )
-{
-	$seed = str_repeat( 0, 16 );
-	for ( $i = 0; $i < 16; $i++ )
-		$seed[ $i ] = rand( 0, 255 );
-	$hash = hash_hmac( "sha256", $username, $seed, 'true' );
+include 'authentication.php';
 
-	return '$' . base64_encode( $seed ) . '$' . base64_encode( $hash );
+if ( $argc != 2 )	{
+	echo "Usage: $argv[0] <username>\n\n";
+	exit( 1 );
 }
 
-
-function CRAMresponse( $password, $challenge )
-{
-	if ( $challenge[ 0 ] != '$' )
-		return 'invalid challenge format';
-	$challenge = ltrim( $challenge, '$' );
-	$chlngPart = explode( '$', $challenge );
-	if ( count( $chlngPart ) != 2 )
-		return 'invalid challenge format';
-	$salt = base64_decode( $chlngPart[ 0 ] );
-	$chlng = base64_decode( $chlngPart[ 1 ] );
-	if ( strlen( $chlng ) != 64 )
-		return 'invalid challenge length';
-	$passwd = hash_pbkdf2( "sha1", $password, $salt, 10589, 48, 'true' );
-	if ( strlen( $passwd ) > 64 )	{
-		$response = hash( "sha512", $passwd );
-	}
-	else	{
-		$response = str_repeat( 0x3c, 64 );
-		for ( $i = 0; $i < count( $passwd ); $i++ )
-			$response[ $i ] = $passwd[ $i ];
-	}
-	for ( $i = 0; $i < 64; $i++ )
-		$response[ $i ] = $response[ $i ] ^ $challenge[ $i ];
-	return base64_encode( hash( "sha256", $response ));
-}
+echo userHash( 'testUser' ), "\n";
 
 ?>
 
