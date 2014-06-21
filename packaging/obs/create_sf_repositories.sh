edf4464d71c43444c52b1ecbc52171949069959e
Andreas Baumann <abaumann@yahoo.com>
2014-06-21 18:56:02 +0200

    fedora 19/20 repos

--- packaging/obs/create_sf_repositories.sh
+++ packaging/obs/create_sf_repositories.sh
@@ -3,6 +3,7 @@
 if test $# != 1; then
 	echo "Usage: packaging/obs/create_sf_repositories.sh <release>" 1>&2
 	echo "where release is CentOS-6, CentOS-5, RHEL-6, RHEL-5" 1>&2
+	echo "                 Fedora-20, Fedora-19" 1>&2
 	echo "                 ArchLinux" 1>&2
 	exit 1
 fi
@@ -12,6 +13,78 @@ SOURCEFORGE_ROOT=/mnt/sf
 BASE=$PWD
 
 DISTRO=$1
+
+if test $DISTRO = "Fedora-20"; then
+
+	REPODIR=/mnt/sf/repositories/Fedora-20
+	
+	rm -vrf $REPODIR
+	mkdir -p $REPODIR
+
+	wget --no-parent -m http://download.opensuse.org/repositories/home:/wolframe_user/Fedora_20/
+	cd $BASE/download.opensuse.org/repositories/home:/wolframe_user/Fedora_20/
+
+	sed 's|http://download.opensuse.org/repositories/home:/wolframe_user/|http://sourceforge.net/projects/wolframe/files/repositories/|g' \
+		home:wolframe_user.repo | \
+	sed 's|\[home_wolframe_user\]|[Wolframe]|g' | \
+	sed 's|wolframe_user.s Home Project|Wolframe Project|g' | \
+	sed 's|Fedora_20|Fedora-20|g' \
+		> $REPODIR/wolframe.repo
+
+	mkdir -p $REPODIR/repodata
+
+	cp repodata/repomd.xml.key $REPODIR/repodata
+	cp repodata/repomd.xml $REPODIR/repodata
+	cp repodata/*primary.xml.gz $REPODIR/repodata
+	cp repodata/*filelists.xml.gz $REPODIR/repodata
+	cp repodata/*other.xml.gz $REPODIR/repodata
+
+	mkdir -p $REPODIR/x86_64
+	cp -v x86_64/*.rpm $REPODIR/x86_64/.
+	mkdir -p $REPODIR/i686
+	cp -v i686/*.rpm $REPODIR/i686/.
+	mkdir -p $REPODIR/src
+	cp -v src/*.src.rpm $REPODIR/src/.
+	
+	cd $BASE
+	rm -rf download.opensuse.org
+fi
+
+if test $DISTRO = "Fedora-19"; then
+
+	REPODIR=/mnt/sf/repositories/Fedora-19
+	
+	rm -vrf $REPODIR
+	mkdir -p $REPODIR
+
+	wget --no-parent -m http://download.opensuse.org/repositories/home:/wolframe_user/Fedora_19/
+	cd $BASE/download.opensuse.org/repositories/home:/wolframe_user/Fedora_19/
+
+	sed 's|http://download.opensuse.org/repositories/home:/wolframe_user/|http://sourceforge.net/projects/wolframe/files/repositories/|g' \
+		home:wolframe_user.repo | \
+	sed 's|\[home_wolframe_user\]|[Wolframe]|g' | \
+	sed 's|wolframe_user.s Home Project|Wolframe Project|g' | \
+	sed 's|Fedora_19|Fedora-19|g' \
+		> $REPODIR/wolframe.repo
+
+	mkdir -p $REPODIR/repodata
+
+	cp repodata/repomd.xml.key $REPODIR/repodata
+	cp repodata/repomd.xml $REPODIR/repodata
+	cp repodata/*primary.xml.gz $REPODIR/repodata
+	cp repodata/*filelists.xml.gz $REPODIR/repodata
+	cp repodata/*other.xml.gz $REPODIR/repodata
+
+	mkdir -p $REPODIR/x86_64
+	cp -v x86_64/*.rpm $REPODIR/x86_64/.
+	mkdir -p $REPODIR/i686
+	cp -v i686/*.rpm $REPODIR/i686/.
+	mkdir -p $REPODIR/src
+	cp -v src/*.src.rpm $REPODIR/src/.
+	
+	cd $BASE
+	rm -rf download.opensuse.org
+fi
 
 if test $DISTRO = "CentOS-6"; then
 
